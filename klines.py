78eb332d54547d7a01b6d451b1e6a6f11dd83c07
Amro Diab <adiab@hotmail.co.uk>
2018-01-09 20:00:15 +0000

    turn function into generator to capture returns

--- klines.py
+++ klines.py
@@ -2,18 +2,25 @@
 # PYTHON_ARGCOMPLETE_OK
 #pylint: disable=no-member
 
+"""
+Get ohlc (Open, High, Low, Close) values from given cryptos
+and detect trends using candlestick patterns
+
+"""
+
+from __future__ import print_function
 import json
 import os
 import sys
 import argparse
-import pandas as pd
-import numpy as np
+import time
+import calendar
+import pandas
+import numpy
 import binance
 import talib
 import argcomplete
-from graph import *
-import time
-import calendar
+from graph import create_graph
 
 HOME_DIR = os.path.expanduser('~')
 CONFIG = json.load(open(HOME_DIR + '/.binance'))
@@ -24,11 +31,7 @@ binance.set(API_KEY, API_SECRET)
 
 def make_float(arr):
     """Convert dataframe array into float array"""
-    return np.array([float(x) for x in arr.values])
-
-
-def get_ohlc():
-    raw = binance.klines(pair, "1m")
+    return numpy.array([float(x) for x in arr.values])
 
 def get_details(pair, args):
     """ Get details from binance API """
@@ -37,55 +40,58 @@ def get_details(pair, args):
     white = "\033[0m"
     raw = binance.klines(pair, "1m")
     epoch = calendar.timegm(time.gmtime())
-    df = pd.DataFrame.from_dict(raw)
+    dataframe = pandas.DataFrame.from_dict(raw)
 
-    OHLC = (make_float(df.open), make_float(df.high), make_float(df.low), make_float(df.close))
+    ohlc = (make_float(dataframe.open),
+            make_float(dataframe.high),
+            make_float(dataframe.low),
+            make_float(dataframe.close))
 
     # LAST 10 items
-    HAMMER = talib.CDLHAMMER(*OHLC)[-10:]
-    INVHAMMER = talib.CDLINVERTEDHAMMER(*OHLC)[-10:]
-    ENGULF = talib.CDLENGULFING(*OHLC)[-10:]
-    DOJI = talib.CDLDOJI(*OHLC)[-10:]
-    print "HAMMER", HAMMER
-    print "INVHAMMER", INVHAMMER
-    print "ENGULF", ENGULF
-    print "DOJI", DOJI
-    print "https://uk.tradingview.com/symbols/{0}/".format(pair)
+    hammer = talib.CDLHAMMER(*ohlc)[-10:]
+    invhammer = talib.CDLINVERTEDHAMMER(*ohlc)[-10:]
+    engulf = talib.CDLENGULFING(*ohlc)[-10:]
+    doji = talib.CDLDOJI(*ohlc)[-10:]
+    yield "HAMMER", hammer
+    yield "INVHAMMER", invhammer
+    yield "ENGULF", engulf
+    yield "DOJI", doji
+    yield "https://uk.tradingview.com/symbols/{0}/".format(pair)
     sys.stdout.write(red)
-    if HAMMER[-1] == 100:
-        print "BUY ", pair, epoch
-    elif INVHAMMER[-1] == 100:
-        print "SELL ", pair,epoch
-    elif ENGULF[-1] == 100:
-        print "BUY ", pair,epoch
-    elif ENGULF[-1] == -100:
-        print "SELL", pair,epoch
-    elif DOJI[-1] == 100:
-        print "INVESTIGATE", pair,epoch
+    if hammer[-1] == 100:
+        yield "BUY ", pair, epoch
+    elif invhammer[-1] == 100:
+        yield "SELL ", pair, epoch
+    elif engulf[-1] == 100:
+        yield "BUY ", pair, epoch
+    elif engulf[-1] == -100:
+        yield "SELL", pair, epoch
+    elif doji[-1] == 100:
+        yield "INVESTIGATE", pair, epoch
     else:
-        print "HOLD ", pair,epoch
+        yield "HOLD ", pair, epoch
     sys.stdout.write(white)
 
     if args.graph:
         #unhash to see graph
-         create_graph(df, pair)
-
+        create_graph(dataframe, pair)
 
 def main():
+    """ main function """
     parser = argparse.ArgumentParser()
     parser.add_argument('-g', '--graph', action='store_true', default=False)
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
-    pairs = ["XRPBTC", "XRPETH", "MANABTC", "PPTBTC", "MTHBTC", "BNBBTC", "BNBETH", "ETHBTC" ]
+    pairs = ["XRPBTC", "XRPETH", "MANABTC", "PPTBTC", "MTHBTC", "BNBBTC", "BNBETH", "ETHBTC"]
     #pairs =  [x for x in binance.prices().keys() if 'BTC' in x]
 
     for pair in pairs:
         #PAIR = "XRPBTC"
         #PAIR = "XRPETH"
         #PAIR = "MANABTC"
-        get_details(pair, args)
-        print
+        for data in get_details(pair, args):
+            print(*data)
 
 if __name__ == '__main__':
     main()
