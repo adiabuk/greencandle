6a4dd1e5ffe99385478ed78faaa9766e69b16f94
Amro Diab <adiab@hotmail.co.uk>
2018-01-21 22:47:08 +0000

    get support/resistance range in points

--- support_resistance.py
+++ support_resistance.py
@@ -8,15 +8,39 @@ from backend import make_float
 from lib.supres import supres
 from lib.binance_common import get_binance_klines
 
+def pip_calc(open, close):
+    if str(open).index('.') >= 3:  # JPY pair
+        multiplier = 0.01
+    else:
+        multiplier = 0.0001
+
+    pips = round((close - open) / multiplier)
+    return int(pips)
+
 def main():
     """Main function"""
+    data = []
+    pairs = binance.prices()
 
-    dataframe = get_binance_klines("XRPETH", interval="5m")
-    close_values = make_float(dataframe.close)
-    support, resistance = supres(close_values, 10)
-    print("resistance:", resistance)
-    print("current:", binance.prices()["XRPETH"])
-    print("support:", support)
+    prices = binance.prices()
+    for pair in pairs:
+        dataframe = get_binance_klines(pair, interval="5m")
+        close_values = make_float(dataframe.close)
+        support, resistance = supres(close_values, 10)
+        #print("resistance:", resistance)
+        #print("current:", binance.prices()["XRPETH"])
+
+        #print("support:", support)
+        try:
+            value = (pip_calc(support[-1], resistance[-1]))
+        except IndexError:
+            print("Skipping", pair)
+            continue
+        data.append([pair, value, support, resistance])
+
+    data.sort(key=lambda x: x[1])
+    for value in data:
+        print(value[0], value[1], value[2], prices[value[0]], value[3])
 
 if __name__ == '__main__':
     main()
