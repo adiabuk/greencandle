465400f4de220521f442771d27a758831f7f3dd4
Amro Diab <adiab@hotmail.co.uk>
2018-02-14 16:52:51 +0000

    Fetch all available data for a pair (binance)

--- lib/binance_common.py
+++ lib/binance_common.py
@@ -16,3 +16,29 @@ def get_binance_klines(pair, interval="1m"):
         sys.exit(2)
     dataframe = pandas.DataFrame.from_dict(raw)
     return dataframe
+
+def get_all_klines(symbol, interval=None):
+    """
+    Get all available data for a trading pair
+    We are limited to 500 entries per request, so we will loop over until we have fetched all
+    entries
+    """
+
+    result = []
+    start_time = 0  # Initial start time - as far back as data is available
+
+    while True:
+        current_section = binance.klines(symbol, interval, startTime=start_time)
+        result += current_section
+
+        # Start time becomes 1 more than start time of last entry, +1, so that we don't duplicate
+        # entries
+        start_time = current_section[-1]['openTime'] + 1
+        if len(current_section) < 500:
+            # Break out of while true loop as we have exhausted possible entries
+            break
+
+    return result
+
+
+
