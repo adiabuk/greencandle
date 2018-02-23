63c7ffc0c8fb9ab1a30bc30515d2f280baa7ca65
Amro Diab <adiab@hotmail.co.uk>
2018-02-23 17:25:57 +0000

    updated config and mysql schema

--- greencandle.sql
+++ greencandle.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+-- MySQL dump 10.16  Distrib 10.1.31-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: greencandle
 -- ------------------------------------------------------
--- Server version	5.7.20-1ubuntu1
+-- Server version	10.1.31-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,43 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `balance`
+--
+
+DROP TABLE IF EXISTS `balance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `balance` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ctime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `exchange_id` int(11) unsigned NOT NULL,
+  `gbp` varchar(30) DEFAULT NULL,
+  `btc` varchar(30) DEFAULT NULL,
+  `usd` varchar(30) DEFAULT NULL,
+  `count` varchar(30) DEFAULT NULL,
+  `coin` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `exchange_id` (`exchange_id`),
+  CONSTRAINT `balance_ibfk_2` FOREIGN KEY (`exchange_id`) REFERENCES `exchange` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=489 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `coin`
+--
+
+DROP TABLE IF EXISTS `coin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `coin` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `symbol` varchar(10) DEFAULT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `data`
 --
 
@@ -23,7 +60,7 @@ DROP TABLE IF EXISTS `data`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `data` (
-  `id` int(11) unsigned NOT NULL DEFAULT '0',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `symbol` varchar(10) DEFAULT NULL,
   `ctime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `event` varchar(20) DEFAULT NULL,
@@ -31,18 +68,124 @@ CREATE TABLE `data` (
   `data` varchar(1000) DEFAULT NULL,
   `difference` varchar(5) DEFAULT NULL,
   `resistance` varchar(1000) DEFAULT NULL,
-  `support` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `support` varchar(1000) DEFAULT NULL,
+  `buy` varchar(20) DEFAULT NULL,
+  `sell` varchar(20) DEFAULT NULL,
+  `market` varchar(20) DEFAULT NULL,
+  `balance` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35494 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `data`
+-- Table structure for table `exchange`
 --
 
-LOCK TABLES `data` WRITE;
-/*!40000 ALTER TABLE `data` DISABLE KEYS */;
-/*!40000 ALTER TABLE `data` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `exchange`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exchange` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `historical`
+--
+
+DROP TABLE IF EXISTS `historical`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historical` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pair` varchar(7) DEFAULT NULL,
+  `buckettime` datetime DEFAULT NULL,
+  `low` float DEFAULT NULL,
+  `high` float DEFAULT NULL,
+  `open` float DEFAULT NULL,
+  `close` float DEFAULT NULL,
+  `volume` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pair_2` (`pair`,`buckettime`),
+  KEY `pair` (`pair`),
+  KEY `buckettime` (`buckettime`),
+  KEY `volume` (`volume`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary table structure for view `hour_balance`
+--
+
+DROP TABLE IF EXISTS `hour_balance`;
+/*!50001 DROP VIEW IF EXISTS `hour_balance`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `hour_balance` (
+  `exchange_id` tinyint NOT NULL,
+  `usd1` tinyint NOT NULL,
+  `coin` tinyint NOT NULL,
+  `ctime1` tinyint NOT NULL,
+  `ctime2` tinyint NOT NULL,
+  `usd2` tinyint NOT NULL,
+  `USD_diff` tinyint NOT NULL,
+  `GBP_diff` tinyint NOT NULL,
+  `COUNT_diff` tinyint NOT NULL,
+  `perc_change` tinyint NOT NULL,
+  `BTC_diff` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `symbols`
+--
+
+DROP TABLE IF EXISTS `symbols`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `symbols` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `symbol` varchar(10) DEFAULT NULL,
+  `category` varchar(20) DEFAULT NULL,
+  `maximum_leverage` int(11) DEFAULT NULL,
+  `maximum_amount` int(11) DEFAULT NULL,
+  `overnight_charge_long_percent` float DEFAULT NULL,
+  `overnight_charge_short_percent` float DEFAULT NULL,
+  `decimals` int(11) DEFAULT NULL,
+  `timezone` varchar(80) DEFAULT NULL,
+  `timezone_offset` varchar(10) DEFAULT NULL,
+  `open_day` varchar(80) DEFAULT NULL,
+  `open_time` time DEFAULT NULL,
+  `close_day` varchar(80) DEFAULT NULL,
+  `close_time` time DEFAULT NULL,
+  `daily_break_start` time DEFAULT NULL,
+  `daily_break_stop` time DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `symbol` (`symbol`),
+  KEY `category` (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `hour_balance`
+--
+
+/*!50001 DROP TABLE IF EXISTS `hour_balance`*/;
+/*!50001 DROP VIEW IF EXISTS `hour_balance`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `hour_balance` AS select `tt1`.`exchange_id` AS `exchange_id`,`tt1`.`usd` AS `usd1`,`tt1`.`coin` AS `coin`,`tt1`.`ctime` AS `ctime1`,`tt2`.`ctime` AS `ctime2`,`tt2`.`usd` AS `usd2`,(`tt1`.`usd` - `tt2`.`usd`) AS `USD_diff`,(`tt1`.`gbp` - `tt2`.`gbp`) AS `GBP_diff`,(`tt1`.`count` - `tt2`.`count`) AS `COUNT_diff`,(((`tt1`.`btc` - `tt2`.`btc`) / `tt1`.`btc`) * 100) AS `perc_change`,(`tt1`.`btc` - `tt2`.`btc`) AS `BTC_diff` from (`balance` `tt1` left join `balance` `tt2` on(((`tt1`.`coin` = `tt2`.`coin`) and (`tt1`.`exchange_id` = `tt2`.`exchange_id`)))) where ((`tt1`.`ctime` > (now() - interval 20 minute)) and (`tt2`.`ctime` < (now() - interval 45 minute)) and (`tt2`.`ctime` > (now() - interval 90 minute))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +196,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-08 22:51:37
+-- Dump completed on 2018-02-23 17:23:13
