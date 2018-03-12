bbcb46342a6f2eba532384218026c381ee491180
Amro Diab <adiab@hotmail.co.uk>
2018-03-12 14:58:47 +0000

    cleanup of profit module

--- profit.py
+++ profit.py
@@ -13,9 +13,20 @@ RATE = 0.00014 # GBP to BTC
 def guess_profit(buy_price, sell_price, investment_gbp):
     """
     Get profit prediction based on initial GBP investment and buy/sell values of currency pair
+
+    Args:
+        buy_price
+        sell_price
+        investment_gbp
+
+    Returns: tuple contiaing the following:
+        profit
+        amount
+        difference
+        perc
     """
 
-    buy_price=float(buy_price)
+    buy_price = float(buy_price)
     sell_price = float(sell_price)
 
     total_buy_btc = investment_gbp * RATE
@@ -25,7 +36,7 @@ def guess_profit(buy_price, sell_price, investment_gbp):
     difference = total_sell_btc - total_buy_btc
     profit = difference *(1/RATE)
     perc = ((sell_price - buy_price)/buy_price)*100
-    return profit, amount,difference,perc
+    return profit, amount, difference, perc
 
 def gbp_to_base(gbp, symbol):
     """
@@ -35,7 +46,7 @@ def gbp_to_base(gbp, symbol):
     BTC => OMG
     """
 
-    usd = gbp * CURRENCY.get_RATE('GBP', 'USD')
+    usd = gbp * CURRENCY.get_rate('GBP', 'USD')
     btc = usd * float(binance.prices()['BTCUSDT'])
     omg = btc * float(binance.prices()[symbol + "BTC"])
     return "{0:.10f}".format(omg)
