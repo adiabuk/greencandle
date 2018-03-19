450d5538712c7ca5abe39a93d3a9307c32ab4e7c
Amro Diab <adiab@hotmail.co.uk>
2018-03-19 13:08:48 +0000

    cleanup investment calc script

--- scripts/investment.py
+++ scripts/investment.py
@@ -5,26 +5,28 @@
 Estimate monthly compound profit from starting investment
 """
 
-
-
-import sys
 import argparse
 import argcomplete
 
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--start', required=True)
-    parser.add_argument('-m', '--months',required=True)
+    parser.add_argument('-s', '--start', required=True,
+                        help="Initial investment")
+    parser.add_argument('-m', '--months', required=True,
+                        help="monber of months to calculate")
+    parser.add_argument('-p', '--percent', required=True,
+                        help="average percent profit per day")
+
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
     total = int(args.start)
     months = int(args.months)
+    percent = float(args.percent)/100
 
     for i in range(0, 31 * months):
         print(i, int(total))
-        total = total+(total * 0.1)
+        total = total+(total * percent)
 
     print(total)
 
