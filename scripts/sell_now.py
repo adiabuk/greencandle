50eaae5f24b073bd9690e7aa0b12895786570409
Amro Diab <adiab@hotmail.co.uk>
2018-04-22 17:12:49 +0000

    some hacky helper scripts

---
+++ scripts/sell_now.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#pylint: disable=wrong-import-position
+
+import calendar
+import os
+import sys
+import time
+import binance
+
+BASE_DIR = os.getcwd().split("greencandle", 1)[0] + "greencandle"
+sys.path.append(BASE_DIR)
+
+from lib.order import sell
+from lib.auth import binance_auth
+
+def main():
+    binance_auth()
+    pair = sys.argv[1]
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    current_price = binance.prices()[pair]
+    print(current_time, current_price)
+    sells = []
+    sells.append((pair, current_time, current_price))
+    sell(sells, test_data=False, test_trade=False, interval="5m")
+
+
+if __name__ == "__main__":
+    main()
