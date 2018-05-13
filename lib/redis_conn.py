cc27234c4942f9842c6b6c53bba0535c366612ca
Amro Diab <adiab@hotmail.co.uk>
2018-05-13 13:37:09 +0000

    revert to 2% and 8% sell trigger

--- lib/redis_conn.py
+++ lib/redis_conn.py
@@ -149,36 +149,43 @@ class Redis(object):
         current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(current_mepoch))
         value = self.dbase.get_trade_value(pair)
 
-        if not value and totals[-1] < -4 and totals[-2] < totals[-1]:
+        if not value and (1 <= totals[-1] <= 50 and
+                          1 <= totals[-2] <= 50 and
+                          float(sum(totals[:3])) / max(len(totals[:3]), 1) < totals[-1]):
+            LOGGER.critical("AMROX8: BUY {0} {1} {2} {3}".format(totals, current_time,
+                                                                 format(float(current_price),
+                                                                        ".20f"),
+                                                                 items[-1]))
 
-            LOGGER.info("BUY {0} {1} {2} {3}".format(totals, current_time,
-                                                     format(float(current_price), ".20f"),
-                                                     items[-1]))
-
+            #self.dbase.insert_trade(pair, current_time, format(float(current_price), ".20f"),
+            #                        investment, "0")
             return "buy", current_time, format(float(current_price), ".20f")
 
-        elif value and float(current_price) > (float(value[0][0]) *((4/100)+1)):
-            # More than 4% over
+        elif value and float(current_price) > (float(value[0]) *((8/100)+1)):
+            # More than 8% over
             LOGGER.info("SELL 4% {0} {1} {2} {3}".format(totals, current_time,
                                                          format(float(current_price),
                                                                 ".20f"),
                                                          items[-1]))
             return "sell", current_time, format(float(current_price), ".20f")
 
-        elif value and ((7 <= totals[-1] <= 50 and
+        elif value and ((-20 <= totals[-1] <= -1 and
                          float(sum(totals[:3])) / max(len(totals[:3]), 1) > totals[-1]) and
-                        float(current_price) > float(value[0][0]) * (2/100)+1):
+                        float(current_price) > float(value[0]) or
+                        float(current_price) > float(value[0]) * (2/100)+1):
             # total is between -1 and -20 and
             # current_price is 2% more than buy price
+
 
             LOGGER.info("SELL 2% {0} {1} {2} {3}".format(totals, current_time,
                                                          format(float(current_price), ".20f"),
                                                          items[-1]))
             return "sell", current_time, format(float(current_price), ".20f")
+        """ # FIXME
         elif value and float(current_price) < float(value[0][0]) * (1- (2/100)):  # 2% stop loss
             LOGGER.info("SELL stoploss {0} {1} {2} {3}".format(totals, current_time,
                                                                format(float(current_price), ".20f"),
                                                                items[-1]))
             return "sell", current_time, format(float(current_price), ".20f")
-
+        """
         return "hold", current_time, format(float(current_price), ".20f")
--- scripts/get_mysql_status.py
+++ scripts/get_mysql_status.py
@@ -15,22 +15,23 @@ from lib.auth import binance_auth
 
 prices = binance.prices()
 binance_auth()
-a = Mysql(test=False, interval=sys.argv[1])
+dbase = Mysql(test=False, interval=sys.argv[1])
 
-trades = a.get_trades()
+trades = dbase.get_trades()
 profits = []
 percs = []
 for trade in trades:
-  current_price = float(prices[trade])
-  trade_details = a.get_trade_value(trade)
-  buy_price = float(trade_details[0][0])
-  amount = float(trade_details[0][1])
-  profit = (current_price/0.00014*amount) - (buy_price/0.00014*amount)
-  perc = 100 * (current_price - buy_price) / buy_price
-  profits.append(profit)
-  percs.append(perc)
-  print(trade, buy_price, current_price, current_price > buy_price, perc, profit)
+    current_price = float(prices[trade])
+    trade_details = dbase.get_trade_value(trade)
+    buy_price = float(trade_details[0][0])
+    amount = float(trade_details[0][1])
+    profit = (current_price/0.00014*amount) - (buy_price/0.00014*amount)
+    perc = 100 * (current_price - buy_price) / buy_price
+    profits.append(profit)
+    percs.append(perc)
+    #print(trade, buy_price, current_price, current_price > buy_price, perc, profit)
+    print("  pair:{0}, buy:{1}, current:{2}, amount:{3}, in_profit:{4}, perc:{5}, profit:{6}"
+          .format(trade, buy_price, current_price, amount, current_price > buy_price, perc, profit))
 print("Total profit: {0}, Avg Profit:{1}, Avg Perc: {2}".format(sum(profits),
-    sum(profits)/len(profits),
-    sum(percs)/len(percs)))
-
+                                                                sum(profits)/len(profits),
+                                                                sum(percs)/len(percs)))
