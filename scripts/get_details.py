894059ed277d5bd279f5c23ba3d14cd8900f4a37
Amro Diab <adiab@hotmail.co.uk>
2018-05-16 22:43:24 +0100

    script for analysis of data

--- scripts/get_details.py
+++ scripts/get_details.py
@@ -14,14 +14,17 @@ from lib.redis_conn import Redis
 
 def get_details(address, action):
     """
-    get detaild for particular address and action
+    get details for particular address and action
     """
     redis = Redis('5m', db=0)
-    details = list(redis.get_details(address))  # KNCBTC BUY
+    details = list(redis.get_details(address))
     for item in details:
         dict_item = ast.literal_eval(item[-1].decode())
-        if dict_item['action'] == action:
+        if dict_item['action'] == int(action):
             print(item[0], dict_item['action'])
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: {0} [address] [action]".format(sys.argv[0]))
+        sys.exit(2)
     get_details(sys.argv[1], sys.argv[2])
