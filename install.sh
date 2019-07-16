6c4e152978be2b3562beee0064d461e7b1480a46
Amro Diab <adiab@hotmail.co.uk>
2019-07-16 22:01:58 +0100

    install script for docker container

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get -y install python3 python3-pip mysql-server libmysqlclient-dev redis
+set -e
+
+apt-get update
+apt-get -y install python3 python3-pip wget make git mysql-client libmysqlclient-dev python3-dev
 
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
-pip install -r requirements.txt
 
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-tar zxvf ta-lib-0.4.0-src.tar.gz -C /tmp
-cd /tmp/ta-lib-0.4.0
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz -P /tmp
+tar zxvf /tmp/ta-lib-0.4.0-src.tar.gz -C /tmp
+cd /tmp/ta-lib
 ./configure --prefix=/usr
 make
 make install
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-
+cd /vagrant
+python setup.py install
