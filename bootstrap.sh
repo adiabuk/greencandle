c852c7f0422cdf9d780b60528b1d54d1c2833009
Amro Diab <adiab@hotmail.co.uk>
2019-07-18 00:00:29 +0100

    cleanup bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 apt-get -y update
-apt-get -y install docker.io
+apt-get -y install docker.io ntpdate
 
 docker build -f /vagrant/Dockerfile-gc . --tag=greencandle-test
-docker build -f /vagrant/Dockerfile-ms . --tag=mysql
-mysql -uroot -pmy-secret-pw < /tmp/greencandle.sql
-docker build -f /vagrant/Dockerfile-rs . --tag=redis
 
-docker run greencandle-test -d
-docker run mysql -d
-docker run redis -d
+docker build -f /vagrant/Dockerfile-ms . --tag=mysql
+docker run -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=password mysql
+
+mysql --protocal tcp -h localhost -uroot -ppassword < /vagrant/greencandle.sql
+docker build -f /vagrant/Dockerfile-rs . --tag=redis
+docker run -p 6379:6379 -d redis
+
+#docker run -d greencandle-test
 
