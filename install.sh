0c09483577f8d2c5420f506aff9b360784f323ab
Amro Diab <adiab@hotmail.co.uk>
2019-07-17 23:59:46 +0100

    cleanup install script

--- install.sh
+++ install.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-apt-get update
-apt-get -y install python3 python3-pip wget make git mysql-client libmysqlclient-dev python3-dev
-
-update-alternatives --install /usr/bin/python python /usr/bin/python3 1
-update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
-
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz -P /tmp
 tar zxvf /tmp/ta-lib-0.4.0-src.tar.gz -C /tmp
 cd /tmp/ta-lib
 ./configure --prefix=/usr
 make
 make install
-cd /vagrant
-python setup.py install
+pip install pip==9.0.1 numpy==1.16.0
+pip install -e git+https://github.com/adiabuk/greencandle.git#egg=greencandle
+echo "Installation Complete"
