d4eaf224378df34d6fabf6bcab6339d58c1a8fd3
Amro Diab <adiab@hotmail.co.uk>
2019-07-18 23:50:58 +0100

    move data restore to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,9 @@ apt-get -y install docker.io docker-compose ntpdate mysql-client
 docker build -f /vagrant/docker/Dockerfile-gc . --tag=greencandle
 docker build -f /vagrant/docker/Dockerfile-ms . --tag=gc-mysql
 docker build -f /vagrant/docker/Dockerfile-rs . --tag=gc-redis
+
+
+cd /vagrant/docker
+docker-compose up -d mysql
+mysql --protocol tcp -h localhost -uroot -ppassword < /tmp/greencandle.sql
+docker-compose up -d
