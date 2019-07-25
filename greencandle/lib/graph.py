bb56ccd1225ced1380b3511b5151d53c48dc97ee
Amro Diab <adiab@hotmail.co.uk>
2019-07-25 23:18:12 +0100

    remove hardcoded indicators

--- greencandle/lib/graph.py
+++ greencandle/lib/graph.py
@@ -8,6 +8,7 @@ import time
 import pickle
 import zlib
 import pandas
+from collections import defaultdict
 from selenium import webdriver
 from pyvirtualdisplay import Display
 import plotly.offline as py
@@ -17,6 +18,7 @@ import plotly.graph_objs as go
 from PIL import Image
 from resizeimage import resizeimage
 from .redis_conn import Redis
+from .config import get_config
 
 PATH = os.getcwd() + "/graphs/in/"
 PATH = '/tmp/'
@@ -46,64 +48,74 @@ def resize_screenshot(filename=None):
             cover = resizeimage.resize_width(image, 120)
             cover.save("{0}/{1}_resized.png".format(PATH, filename), image.format)
 
-def create_graph(dataframe, dataframe2, dataframe3, dataframe4, dataframe5, pair):
+def create_graph(pair, data):
     """Create graph html file using plotly offline-mode from dataframe object"""
     py.init_notebook_mode()
-    dataframe["time"] = pandas.to_datetime(dataframe["closeTime"], unit="ms")
-    candles = go.Candlestick(x=dataframe.time + pandas.Timedelta(hours=1),
-                             open=dataframe.open,
-                             high=dataframe.high,
-                             low=dataframe.low,
-                             close=dataframe.close)
-    ema18 = go.Scatter(x=dataframe2['date'], # assign x as the dataframe column 'x'
-                       y=dataframe2['value'],
-                       name='EMA_18')
-    ema25 = go.Scatter(x=dataframe3['date'], # assign x as the dataframe column 'x'
-                       y=dataframe3['value'],
-                       name='EMA_25')
-    wma7 = go.Scatter(x=dataframe4['date'], # assign x as the dataframe column 'x'
-                      y=dataframe4['value'],
-                      name='WMA_7')
-    events = go.Scatter(x=dataframe5['date'],
-                        y=dataframe5['current_price'],
-                        name="events",
-                        mode='markers+text',
-                        text=dataframe5['result'],
-                        textposition='top center',
-                        marker=dict(size=16, color=dataframe5['result']))
-    graphs = [candles, ema18, ema25, wma7, events]
+    graphs = []
+    for name, value in data.items():
+        if name == 'ohlc':
+            value["time"] = pandas.to_datetime(value["closeTime"], unit="ms")
+            item = go.Candlestick(x=value.time + pandas.Timedelta(hours=1),
+                                  open=value.open,
+                                  high=value.high,
+                                  low=value.low,
+                                  close=value.close)
+        elif name == 'event':
+            item = go.Scatter(x=value['date'],
+                              y=value['current_price'],
+                              name="events",
+                              mode='markers+text',
+                              text=value['result'],
+                              textposition='top center',
+                              marker=dict(size=16, color=value['result']))
+        else:
+            item = go.Scatter(x=value['date'], # assign x as the dataframe column 'x'
+                              y=value['value'],
+                              name=name)
+        graphs.append(item)
     filename = "simple_candlestick_{0}".format(pair)
     py.plot(graphs, filename="{0}/{1}.html".format(PATH, filename), auto_open=False)
 
-def get_data(test=False, db=0):
+def get_data(test=False, db=0, interval='1m'):
     """Fetch data from redis"""
     print('Using db: {0}'.format(db))
     redis = Redis(test=test, db=db)
     list_of_series = []
-    list_of_ema18 = []
-    list_of_ema25 = []
-    list_of_wma7 = []
-    list_of_events = []
-    index = redis.get_items('ETHBTC', '1m')
+    index = redis.get_items('ETHBTC', interval)
+
+    main_indicators = get_config("backend")["indicators"].split()
+    ind_list = []
+    print(main_indicators)
+    for i in main_indicators:
+        split = i.split(';')
+        ind = split[1] + '_' + split[2]
+        ind_list.append(ind)
+
+
+    list_of_results = defaultdict(list)
     for index_item in index:
-        ema18 = ast.literal_eval(redis.get_item(index_item, 'EMA_18').decode())
-        ema25 = ast.literal_eval(redis.get_item(index_item, 'EMA_25').decode())
-        wma7 = ast.literal_eval(redis.get_item(index_item, 'WMA_7').decode())
-        ohlc = ast.literal_eval(redis.get_item(index_item, 'ohlc').decode())['result']
+        result_list = {}
+        for ind in ind_list:
+            result_list[ind] = ast.literal_eval(redis.get_item(index_item, ind).decode())
+        result_list['ohlc'] = ast.literal_eval(redis.get_item(index_item, 'ohlc').decode())['result']
         try:
-            event = ast.literal_eval(redis.get_item(index_item, 'trigger').decode())
-            list_of_events.append((event['result'], event['current_price'], event['date']))
+            result_list['event'] = ast.literal_eval(redis.get_item(index_item, 'trigger').decode())
         except AttributeError:  # no event for this time period, so skip
             pass
-        rehydrated = pickle.loads(zlib.decompress(ohlc))
+        rehydrated = pickle.loads(zlib.decompress(result_list['ohlc']))
         list_of_series.append(rehydrated)
-        list_of_ema18.append((ema18['result'], ema18['date']))
-        list_of_ema25.append((ema25['result'], ema25['date']))
-        list_of_wma7.append((wma7['result'], wma7['date']))
+        for ind in ind_list:
+            list_of_results[ind].append((result_list[ind]['result'], result_list[ind]['date']))
+        try:
+            list_of_results['event'].append((result_list['event']['result'],
+                result_list['event']['current_price'], result_list['event']['date']))
+        except KeyError:
+            pass
+    dataframes = {}
 
-    dataframe = pandas.DataFrame(list_of_series)
-    dataframe2 = pandas.DataFrame(list_of_ema18, columns=['value', 'date'])
-    dataframe3 = pandas.DataFrame(list_of_ema25, columns=['value', 'date'])
-    dataframe4 = pandas.DataFrame(list_of_wma7, columns=['value', 'date'])
-    dataframe5 = pandas.DataFrame(list_of_events, columns=['result', 'current_price', 'date'])
-    return dataframe, dataframe2, dataframe3, dataframe4, dataframe5
+    dataframes['ohlc'] = pandas.DataFrame(list_of_series)
+    dataframes['event'] = pandas.DataFrame(list_of_results['event'], columns=['result',
+        'current_price', 'date'])
+    for ind in ind_list:
+        dataframes[ind] = pandas.DataFrame(list_of_results[ind], columns=['value', 'date'])
+    return dataframes
