a12a2ac63a460aa34737c636dbfae8b0ff538383
Amro Diab <adiab@hotmail.co.uk>
2019-07-26 23:41:55 +0100

    properly setup databases for live and test dbs

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,15 @@ chmod +x /usr/local/bin/docker-compose
 docker build -f /vagrant/Dockerfile-gc . --tag=greencandle
 docker build -f /vagrant/Dockerfile-ms . --tag=gc-mysql
 docker build -f /vagrant/Dockerfile-rs . --tag=gc-redis
-
+echo "127.0.0.1    mysql" >> /etc/hosts
+echo "127.0.0.1    redis" >> /etc/hosts
 
 cd /vagrant/docker
 docker-compose up -d mysql
-mysql --protocol tcp -h localhost -uroot -ppassword < ./greencandle.sql
+mysql --protocol=tcp  -uroot -ppassword  -e "create database greencandle"
+mysql --protocol=tcp  -uroot -ppassword  -e "create database greencandle_test"
+mysql --protocol=tcp  -uroot -ppassword  -e "grant all on *.* to 'greencandle'@'%' identified by 'password' with grant option;"
+
+mysql --protocol tcp -h localhost -uroot -ppassword greencandle < ./greencandle.sql
+mysql --protocol tcp -h localhost -uroot -ppassword greencandle_test < ./greencandle.sql
 docker-compose up -d
