91ddacacb8be532c7782a83328b48421ee77ff77
Amro Diab <adiab@hotmail.co.uk>
2019-07-30 03:01:17 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ apt-get -y update
 apt-get -y install docker.io ntpdate mysql-client
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+[[ ! -f config.ini ]] && cp config.ini.sample config.ini
 
 sudo systemctl unmask docker.service
 sudo systemctl unmask docker.socket
