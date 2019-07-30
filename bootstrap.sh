e7d873e38a9d0e2a5b09ccaa1233039fecaad9f9
Amro Diab <adiab@hotmail.co.uk>
2019-07-30 01:24:50 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ apt-get -y install docker.io ntpdate mysql-client
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+systemctl start docker
 docker build -f ./Dockerfile-gc . --tag=greencandle
 docker build -f ./Dockerfile-ms . --tag=gc-mysql
 docker build -f ./Dockerfile-rs . --tag=gc-redis
