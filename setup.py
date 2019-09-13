c32c17bb77c2ce3ee24159187e6de2bb7cfd369c
Amro Diab <adiab@hotmail.co.uk>
2019-09-13 12:55:04 +0100

    more setup cleanup

--- setup.py
+++ setup.py
@@ -6,14 +6,9 @@ Setup script for greencandle - a trading bot
 """
 
 import glob
+from collections import defaultdict
 import pip
 from setuptools import setup, find_packages
-
-REQUIRES = []
-LINKS = []
-
-REQUIREMENTS = pip.req.parse_requirements(
-    'requirements.txt', session=pip.download.PipSession())
 
 exec(open('greencandle/version.py').read())
 
@@ -35,14 +30,21 @@ def get_shell_scripts():
     """get list of shell scripts in module"""
     return glob.glob('greencandle/scripts/[!_]*[!.py]')
 
-for item in REQUIREMENTS:
-    # we want to handle package names and also repo urls
-    if getattr(item, 'url', None):  # older pip has url
-        LINKS.append(str(item.url))
-    if getattr(item, 'link', None): # newer pip has link
-        LINKS.append(str(item.link))
-    if item.req:
-        REQUIRES.append(str(item.req))
+def get_reqs(req):
+    """get requirements and links from requirements.txt"""
+    results = defaultdict(list)
+    requirements = pip.req.parse_requirements(
+        'requirements.txt', session=pip.download.PipSession())
+
+    for item in requirements:
+        # we want to handle package names and also repo urls
+        if getattr(item, 'url', None):  # older pip has url
+            results['link'].append(str(item.url))
+        elif getattr(item, 'link', None): # newer pip has link
+            results['link'].append(str(item.link))
+        elif item.req:
+            results['requires'].append(str(item.req))
+    return results[req]
 
 setup(
     name='greencandle',
@@ -52,8 +54,8 @@ setup(
     author='Amro Diab',
     author_email='adiab@linuxmail.org',
     url='https://github.com/adiabuk/greencandle',
-    install_requires=REQUIRES,
-    dependency_links=LINKS,
+    install_requires=get_reqs('requires'),
+    dependency_links=get_reqs('links'),
     scripts=get_shell_scripts(),
     entry_points={'console_scripts': get_entrypoints()},
     classifiers=[],
