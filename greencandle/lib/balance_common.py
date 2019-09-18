67fac1213510a3f1b85405e5db7230d89ad04992
Amro Diab <adiab@hotmail.co.uk>
2019-09-18 18:52:10 +0100

    get base currency from pair

--- greencandle/lib/balance_common.py
+++ greencandle/lib/balance_common.py
@@ -10,3 +10,11 @@ def default_to_regular(ddict):
     if isinstance(ddict, defaultdict):
         ddict = {k: default_to_regular(v) for k, v in ddict.items()}
     return ddict
+
+def get_base(pair):
+    """Return BaseCurrency of trading pair"""
+    bases = ['USDT', 'BTC', 'ETH', 'BNB']
+    try:
+        return list(filter(pair.endswith, bases))[0]
+    except IndexError:
+        return None
