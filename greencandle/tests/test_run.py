858166a840e3e5e68848197fbda335570417405e
Amro Diab <adiab@hotmail.co.uk>
2019-09-23 18:14:48 +0100

    fix some unittests

--- greencandle/tests/test_run.py
+++ greencandle/tests/test_run.py
@@ -15,11 +15,11 @@ class TestBNBETH(make_test_case('BNBETH', '2019-02-27', 18, 10, -4.5)):
     """Test BNBETH"""
     pass
 
-class TestLRTBTC(make_test_case('LRCBTC', '2019-01-18', 18, 10, -5)):
+class TestLRTBTC(make_test_case('LRTBTC', '2019-01-18', 18, 10, -5)):
     """Test BNBETH"""
     pass
 
-class TestRCNBNB(make_test_case('RCNBNB', '2019-03-29', 18, 10, -6.5)):
+class TestRCNBNB(make_test_case('RCNBNB', '2019-03-29', 18, 10, -12.5)):
     """Test RCNBNB"""
     pass
 
