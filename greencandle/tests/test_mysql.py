0e2fb1d4a65f84b027a24a2946cff7d5b0f9848e
Amro Diab <adiab@hotmail.co.uk>
2019-09-26 20:45:58 +0100

    fix sql test

--- greencandle/tests/test_mysql.py
+++ greencandle/tests/test_mysql.py
@@ -10,6 +10,7 @@ config.create_config()
 
 from greencandle.lib.logger import getLogger
 from greencandle.lib.mysql import Mysql
+from greencandle.lib.common import perc_diff, add_perc
 from .unittests import OrderedTest
 
 LOGGER = getLogger(__name__, config.main.logging_level)
@@ -38,22 +39,26 @@ class TestMysql(OrderedTest):
         self.date = '2018-05-07 22:44:59'
         self.sell_date = '2018-05-07 22:44:59'
         self.pair = 'XXXYYY'
-        self.dbase.insert_trade(self.pair, self.date, 100, 20, 30)
-
-
-        buy_time, sell_time, pair, interval, buy_price, sell_price, investment, total = \
-                self.dbase.fetch_sql_data('select * from trades')[-1]
+        self.buy_price = 100
+        self.sell_price = 500
+        base_in = 20
+        self.dbase.insert_trade(self.pair, self.date, self.buy_price, base_in, 30)
+        sql = 'select buy_time, sell_time from trades'
+        buy_time, sell_time = self.dbase.fetch_sql_data(sql)[-1]
         current_time = buy_time.strftime("%Y-%m-%d %H:%M:%S")
         assert current_time == self.date
         assert sell_time is None
-        self.dbase.update_trades(self.pair, self.sell_date, 500)
-        buy_time, sell_time, pair, interval, buy_price, sell_price, investment, total = \
-                self.dbase.fetch_sql_data('select * from trades')[-1]
+
+        quote = self.dbase.get_quantity(self.pair)
+        perc_inc = perc_diff(self.buy_price, self.sell_price)
+        base_out = add_perc(perc_inc, base_in)
+        self.dbase.update_trades(self.pair, self.sell_date, self.sell_price, quote=quote,
+                                 base_out=base_out)
+        sell_time = self.dbase.fetch_sql_data('select sell_time from trades')[-1]
         assert sell_time is not None
 
     def tearDown(self):
         del self.dbase
-
 
 if __name__ == '__main__':
     unittest.main()
