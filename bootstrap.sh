195d5c5b1524aaa7063f5a90f356fea27ab7a5af
Amro Diab <adiab@hotmail.co.uk>
2019-10-22 23:08:51 +0100

    remove refs to old config file

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,6 @@ apt-get -y update
 apt-get -y install docker.io ntpdate mysql-client screen atop jq iotop
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-[[ ! -f config.ini ]] && cp config.ini.sample config.ini
 
 sudo systemctl unmask docker.service
 sudo systemctl unmask docker.socket
