6d8716589fa02cacf90e8940337b55d4ff9c98a1
Amro Diab <adiab@hotmail.co.uk>
2019-11-06 21:29:36 +0000

    allow for high highs and lower lows with new strategy

--- greencandle/tests/test_run.py
+++ greencandle/tests/test_run.py
@@ -6,19 +6,19 @@ Unittest file for testing results of a run using downloaded data
 import unittest
 from .unittests import make_test_case
 
-class TestBTCUSDT(make_test_case('BTCUSDT', '2019-05-05', 18, 10, -4.5)):
+class TestBTCUSDT(make_test_case('BTCUSDT', '2019-05-05', 27, 11, -4.6)):
     """Test BTCUSDT"""
     pass
 
-class TestBNBETH(make_test_case('BNBETH', '2019-02-27', 18, 10, -4.5)):
+class TestBNBETH(make_test_case('BNBETH', '2019-02-27', 35, 14, -5.5)):
     """Test BNBETH"""
     pass
 
-class TestLRCBTC(make_test_case('LRCBTC', '2019-01-08', 50, 50, -5.5)):
+class TestLRCBTC(make_test_case('LRCBTC', '2019-01-08', 20, 12, -8)):
     """Test LRCBTC"""
     pass
 
-class TestRCNBNB(make_test_case('RCNBNB', '2019-03-29', 18, 10, -12.5)):
+class TestRCNBNB(make_test_case('RCNBNB', '2019-03-29', 24, 23, -5)):
     """Test RCNBNB"""
     pass
 
