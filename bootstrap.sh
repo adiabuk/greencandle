3a579c97c913cffa5799142b6e5c0b70942ac68a
Amro Diab <adiab@hotmail.co.uk>
2019-11-09 16:23:47 +0000

    send docker logs to syslog

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,18 @@ apt-get -y install docker.io ntpdate mysql-client screen atop jq iotop ntp
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+cat > /etc/docker/daemon.json << EOF
+{
+  "live-restore": true,
+  "log-driver": "syslog",
+  "raw-logs": true,
+  "log-opts": {
+    "syslog-facility": "local1",
+    "tag": "{{.Name}}"
+  }
+}
+EOF
+
 systemctl start ntp
 systemctl unmask docker.service
 systemctl unmask docker.socket
