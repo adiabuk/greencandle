62511d106295f018dfb91f1ba009bb4a45604335
Amro Diab <adiab@hotmail.co.uk>
2019-11-10 17:48:19 +0000

    wait for dependencies before starting main app

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,17 @@ if [[ -z $CONFIG_ENV ]]; then
   exit 1
 fi
 
+
+while ! nc -z mysql 3306; do
+  echo Waiting for mysql;
+  sleep 1;
+done
+
+while ! nc -z redis 6379; do
+  echo Waiting for redis;
+  sleep 1;
+done
+
 configstore package process_templates --ignore-role --basedir /opt/config $CONFIG_ENV /opt/output
 cp /opt/output/greencandle.ini /etc/greencandle.ini
 
