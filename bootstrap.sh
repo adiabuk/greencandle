902942769042324dc34c8ad626eb2bf4d1701361
Amro Diab <adiab@hotmail.co.uk>
2019-11-12 00:48:44 +0000

    Make host's hostname available to containers for use later

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,7 @@ apt-get -y update
 apt-get -y install docker.io ntpdate mysql-client screen atop jq iotop ntp
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+echo "export HOSTNAME >> ~/.bashrc"
 
 if [[ ! -f /usr/local/bin/configstore ]]; then
     wget https://github.com/motns/configstore/releases/download/v2.4.0/configstore-2.4.0-linux-amd64.tar.gz -P /tmp
