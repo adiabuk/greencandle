fd3abca296fddeda929555b30d4858313132e845
Amro Diab <adiab@hotmail.co.uk>
2019-11-13 13:40:03 +0000

    fix sql perms

--- docker-entrypoint-mysql.sh
+++ docker-entrypoint-mysql.sh
@@ -8,6 +8,7 @@ fi
 
 configstore package process_templates --ignore-role --basedir /opt/config $CONFIG_ENV /opt/output
 cp /opt/output/*.sql /docker-entrypoint-initdb.d
+chown mysql.mysql /docker-entrypoint-initdb.d/*
 
 set -eo pipefail
 shopt -s nullglob
