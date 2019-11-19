f4c69a606ff0d830eea8e415dc63de6ab99d0788
Amro Diab <adiab@hotmail.co.uk>
2019-11-19 21:01:37 +0000

    increase max db connections

--- config/template/perms.sql
+++ config/template/perms.sql
@@ -1,3 +1,4 @@
 CREATE USER 'greencandle'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'greencandle'@'%' WITH GRANT OPTION;
 SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';
+SET GLOBAL max_connections = 1000;
