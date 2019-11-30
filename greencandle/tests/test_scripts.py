2ded556dbaaf1cb406cb4c9c8f31572a522698d1
Amro Diab <adiab@hotmail.co.uk>
2019-11-30 21:30:25 +0000

    Add unittests to run all executables are available and runnable

---
+++ greencandle/tests/test_scripts.py
@@ -0,0 +1,25 @@
+
+"""Test scripts"""
+
+import unittest
+import glob
+import os
+
+class TestScripts(unittest.TestCase):
+    """Test executables included in package"""
+
+    def test_python_scripts(self):
+        """
+        test python executables in scripts dir
+        """
+        entrypoints = []
+        files = glob.glob('greencandle/scripts/[!_]*.py')
+        for filename in files:
+            path = filename.rstrip('.py').replace('/', '.')
+            name = path.split('.')[-1]
+            string = "{0}={1}:main".format(name, path)
+            entrypoints.append(string)
+            print("Running {}".format(name))
+            result = os.system("{} --help >/dev/null 2>&1".format(name))
+            self.assertEqual(result, 0)
+        return entrypoints
