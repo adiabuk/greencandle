eab56e3f57bf055149ccdad8754f864976787968
Amro Diab <adiab@hotmail.co.uk>
2019-12-02 00:53:31 +0000

    add basic automated linting with low pass rate

---
+++ greencandle/tests/test_lint.py
@@ -0,0 +1,25 @@
+
+"""Test lint of python files"""
+
+import unittest
+from glob import glob
+from pathlib import Path
+from pylint.lint import Run
+
+class TestLint(unittest.TestCase):
+    """Test Lint Unittest"""
+
+    def test_lint(self):
+        """
+        test lint
+        """
+        for filename in Path('greencandle').rglob('*.py'):
+            count = len(open(str(filename)).readlines())
+            if 'test' not in str(filename) and count > 5:
+                results = Run([str(filename)], do_exit=False)
+                try:
+                    print(filename)
+                    score = results.linter.stats['global_note']
+                    self.assertGreaterEqual(score, 5)
+                except KeyError:
+                    pass
