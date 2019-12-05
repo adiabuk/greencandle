aa83cda44e19bbfac805be3f43b55848c94746a5
Amro Diab <adiab@hotmail.co.uk>
2019-12-05 00:15:42 +0000

    allow graph script to get open trades from db

--- greencandle/scripts/create_graph.py
+++ greencandle/scripts/create_graph.py
@@ -10,22 +10,34 @@ import argcomplete
 from greencandle.lib import config
 from greencandle.lib.graph import Graph
 config.create_config()
+from greencandle.lib.mysql import Mysql
 
 def main():
     """Main function"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--db", required=True)
-    parser.add_argument("-p", "--pair", required=True)
-    parser.add_argument("-i", "--interval", required=True)
+    parser.add_argument("-a", "--active_pairs", action="store_true", required=False, default=False)
+    parser.add_argument("-p", "--pair", required=False)
+    parser.add_argument("-i", "--interval", required=False)
     parser.add_argument("-t", "--test", action="store_true", default=False, required=False)
     parser.add_argument("-o", "--output_dir", required=True)
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
 
-    graph = Graph(test=args, pair=args.pair, db=args.db, interval=args.interval)
-    graph.get_data()
-    graph.create_graph(args.output_dir)
+    if args.active_pairs:
+        dbase = Mysql()
+        results = dbase.fetch_sql_data("select pair, `interval` from trades where "
+                                       "sell_price is NULL", header=False)
+
+        for pair, interval in results:
+            graph = Graph(test=args, pair=pair, db=args.db, interval=interval)
+            graph.get_data()
+            graph.create_graph(args.output_dir)
+    else:
+        graph = Graph(test=args, pair=args.pair, db=args.db, interval=args.interval)
+        graph.get_data()
+        graph.create_graph(args.output_dir)
 
 if __name__ == '__main__':
     main()
