162128bae5d875c6071e701bced4872a45ce8f7b
Amro Diab <adiab@hotmail.co.uk>
2019-12-06 22:31:47 +0000

    more steps to prod bootstrap

--- install/bootstrap_prod.sh
+++ install/bootstrap_prod.sh
@@ -9,7 +9,8 @@ fi
 
 # Setup local env
 apt-get -y update
-apt-get -y install docker.io ntpdate mysql-client screen atop jq iotop ntp awscli vim
+apt-get -y install docker.io ntpdate mysql-client screen atop jq iotop ntp awscli vim atop htop
+apt-get dist-upgrade -y
 curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 echo "export HOSTNAME >> ~/.bashrc"
@@ -26,13 +27,25 @@ cat > /etc/docker/daemon.json << EOF
 }
 EOF
 
+# Start services
 systemctl start ntp
 systemctl unmask docker.service
 systemctl unmask docker.socket
 systemctl start docker.service
+systemctl start atop
 
 usermod -aG docker ubuntu || true
 
 # Create shared volume
 docker volume create data
 mkdir -p /data/{mysql,config,graphs,report}
+
+# Create and mount swap
+fallocate -l 1G /swapfile
+dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+
+reboot
