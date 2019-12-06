cb69bddd89b98cf19a485020b0822c6173408abf
Amro Diab <adiab@hotmail.co.uk>
2019-12-06 21:14:12 +0000

    Catch divide by zero exception

--- greencandle/scripts/get_active_trades.py
+++ greencandle/scripts/get_active_trades.py
@@ -11,6 +11,9 @@ from ..lib import config
 
 config.create_config()
 from ..lib.auth import binance_auth
+from ..lib.logger import getLogger
+
+LOGGER = getLogger(__name__, config.main.logging_level)
 
 def main():
     """ Main function """
@@ -22,16 +25,23 @@ def main():
     trades = dbase.fetch_sql_data("select pair, buy_time, buy_price from trades where "
                                   "sell_price is NULL", header=False)
     for trade in trades:
-        pair = trade[0]
-        buy_time = trade[1]
-        buy_price = trade[2]
-        current_price = prices[pair]
-        perc = 100 * (float(current_price) - float(buy_price)) / float(buy_price)
-        insert = ('insert into open_trades (pair, buy_time, buy_price, current_price, perc) '
-                  'VALUES ("{0}", "{1}", "{2}", "{3}", "{4}")'.format(pair, buy_time,
-                                                                      buy_price,
-                                                                      current_price, perc))
-        dbase.run_sql_query(insert)
+        try:
+            pair = trade[0]
+            buy_time = trade[1]
+            buy_price = trade[2]
+            current_price = prices[pair]
+            perc = 100 * (float(current_price) - float(buy_price)) / float(buy_price)  #FIXME
+            insert = ('insert into open_trades (pair, buy_time, buy_price, current_price, perc) '
+                      'VALUES ("{0}", "{1}", "{2}", "{3}", "{4}")'.format(pair, buy_time,
+                                                                          buy_price,
+                                                                          current_price, perc))
+            dbase.run_sql_query(insert)
+        except ZeroDivisionError:
+            LOGGER.critical("{} has a zero buy price, unable to calculate percentage".format(pair))
+
+
+
+
 
 if __name__ == "__main__":
     main()
