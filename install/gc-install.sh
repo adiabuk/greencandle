bb66bee269c0601352a2a135a2aa5951aa2b8646
Amro Diab <adiab@hotmail.co.uk>
2019-12-09 21:32:21 +0000

    reinstall pip/python after removing dir

--- install/gc-install.sh
+++ install/gc-install.sh
@@ -17,7 +17,8 @@ else
     cp -rv config /opt
     apt-get update
     rm -rf /usr/local/lib/python3.6/dist-packages/
-    apt-get -y install python3 python3-pip wget make git mysql-client libmysqlclient-dev \
+    sudo apt-get install python3 python3-pip --reinstall
+    apt-get -y install wget make git mysql-client libmysqlclient-dev \
       python3-dev xvfb firefox redis-tools cron vim bsdmainutils libssl-dev
     update-alternatives --install /usr/bin/python python /usr/bin/python3 1
     update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
