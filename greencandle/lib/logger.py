ca4d1ceb5921fe48f2cbae00622b60ec1a7f3936
Amro Diab <adiab@hotmail.co.uk>
2019-12-15 23:44:33 +0000

    format exceptions in journald mode

--- greencandle/lib/logger.py
+++ greencandle/lib/logger.py
@@ -1,23 +1,33 @@
-#pylint: disable=invalid-name,global-statement,protected-access,no-member
+#pylint: disable=invalid-name,no-member,arguments-differ
 
 """
 Generic logging class for greencandle modules
 """
 
-import sys
 import logging
 from systemd.journal import JournaldLogHandler
 from . import config
 
+class OneLineFormatter(logging.Formatter):
+    """logging formatter for exceptions"""
+    def formatException(self, exc_info):
+        result = super().formatException(exc_info)
+        return repr(result)
+
+    def format(self, record):
+        result = super().format(record)
+        if record.exc_text:
+            result = result.replace("\n", "")
+        return result
+
 def getLogger(logger_name=None):
     """
     Get Customized logging instance
-        Args:
-            logger_name: name of module
-        Returns:
-            logging instance with formatted handler
-        """
-
+      Args:
+        logger_name: name of module
+      Returns:
+        logging instance with formatted handler
+    """
     logger = logging.getLogger(logger_name)
     if logger.hasHandlers():
         logger.handlers.clear()
@@ -27,13 +37,20 @@ def getLogger(logger_name=None):
     if config.main.logging_output == "journald":
         handler = JournaldLogHandler()
         #handler = logging.StreamHandler()
-        formatter = logging.Formatter('[%(levelname)s] %(message)s')
+
+        formatter = OneLineFormatter('[%(levelname)s] %(message)s')
+        #formatter = logging.Formatter('[%(levelname)s] %(message)s')
+
         #formatter = logging.Formatter(logging.BASIC_FORMAT)
         handler.setFormatter(formatter)
 
     else:
+        #handler = OneLineExceptionFormatter()
         handler = logging.StreamHandler()
-        formatter = logging.Formatter("%(levelname)s %(name)s %(message)s")
+        #formatter = logging.Formatter("%(levelname)s %(name)s %(message)s")
+        #formatter = OneLineExceptionFormatter("%(levelname)s %(name)s %(message)s")
+        formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s",
+                                      "%Y-%m-%d %H:%M:%S")
         handler.setFormatter(formatter)
 
     logger.addHandler(handler)
@@ -48,10 +65,10 @@ def get_decorator(errors=(Exception,)):
             try:
                 return func(*args, **kwargs)
             except errors:
-                logger.critical("Got Error %s %s", str(sys.exc_info()), errors)
+                logger.exception("Got Error: ")   # %s %s", str(sys.exc_info()), errors)
                 #logger.critical('Function', method.__name__, 'time:', round((te -ts)*1000,1), 'ms')
 
-                raise
+#                raise
 
         return new_func
 
