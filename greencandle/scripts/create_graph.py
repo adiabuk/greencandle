c6a77159b02d03858285a7511b70ae08e3da8a02
Amro Diab <adiab@hotmail.co.uk>
2019-12-26 17:33:43 +0400

    create graphs for all pairs

--- greencandle/scripts/create_graph.py
+++ greencandle/scripts/create_graph.py
@@ -17,7 +17,7 @@ def main():
     """Main function"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--db", required=True)
-    parser.add_argument("-a", "--active_pairs", action="store_true", required=False, default=False)
+    parser.add_argument("-a", "--all_pairs", action="store_true", required=False, default=False)
     parser.add_argument("-p", "--pair", required=False)
     parser.add_argument("-i", "--interval", required=False)
     parser.add_argument("-t", "--test", action="store_true", default=False, required=False)
@@ -26,13 +26,13 @@ def main():
     args = parser.parse_args()
 
 
-    if args.active_pairs:
+    if args.all_pairs:
         dbase = Mysql()
         results = dbase.fetch_sql_data("select pair, `interval` from trades where "
                                        "sell_price is NULL", header=False)
-
-        for pair, interval in results:
-            graph = Graph(test=args, pair=pair, db=args.db, interval=interval)
+        pairs = config.main.pairs
+        for pair in pairs:
+            graph = Graph(test=args, pair=pair, db=args.db, interval=args.interval)
             graph.get_data()
             graph.create_graph(args.output_dir)
     else:
