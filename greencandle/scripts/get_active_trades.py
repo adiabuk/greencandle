eee43b5acb857930afd7d4706fb366974e95665b
Amro Diab <adiab@hotmail.co.uk>
2020-02-22 23:50:28 +0000

    fix lint

--- greencandle/scripts/get_active_trades.py
+++ greencandle/scripts/get_active_trades.py
@@ -12,9 +12,9 @@ config.create_config()
 from ..lib.mysql import Mysql
 
 def main():
-  """ Main function """
-  if len(sys.argv) > 1 and sys.argv[1] == '--help':
-    print("Get list of current trades from mysql")
+    """ Main function """
+    if len(sys.argv) > 1 and sys.argv[1] == '--help':
+        print("Get list of current trades from mysql")
     sys.exit(0)
     dbase = Mysql()
     dbase.get_active_trades()
