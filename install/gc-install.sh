795642a872cbef6a8e7495b9cb1238ffbdce2f46
Amro Diab <adiab@hotmail.co.uk>
2020-02-23 22:09:14 +0000

    use python 3.7.0 for test env

--- install/gc-install.sh
+++ install/gc-install.sh
@@ -58,7 +58,7 @@ pip install pip==9.0.1 numpy==1.16.0
 [[ ! -d $install_dir ]] && ln -s /home/travis/build/adiabuk/greencandle/ $install_dir
 cd $install_dir
 
-pip install . --src /tmp
+pip install -r requirements.txt --src /tmp
 [[ ! -d /opt/output ]] && mkdir /opt/output
 apt-get purge -y gcc g++ g++-8 gcc-8 libx265-165 mercurial-common || true
 apt-get -y autoremove
