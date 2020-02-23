91a693a060814babd7dc233b51d8c885933dd3f1
Amro Diab <adiab@hotmail.co.uk>
2020-02-23 22:22:05 +0000

    remove old pyenv in travis

--- install/bootstrap_dev.sh
+++ install/bootstrap_dev.sh
@@ -20,6 +20,7 @@ apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
+rm -rf /opt/pyenv ||true  # for travisci
 curl https://pyenv.run | bash
 
 cat << \EOF >> ~/.bashrc
