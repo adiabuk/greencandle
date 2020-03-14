ff91ee9f01d7843ac18dbab2f03883fbe6ceb91a
Amro Diab <adiab@hotmail.co.uk>
2020-03-14 16:45:44 +0000

    don't suppress graph log

--- greencandle/scripts/run_in_docker.sh
+++ greencandle/scripts/run_in_docker.sh
@@ -14,6 +14,6 @@ date=`date +"%Y-%m-%d"`
 mkdir -p ${base_dir}/${YEAR}
 echo $PAIR $date
 backend_test -d /data/altcoin_historical/${YEAR}/year/ -s -i 4h -p $PAIR &> ${base_dir}/${YEAR}/${PAIR}_${date}.log
-create_graph -d1 -p $PAIR -i 4h -o ${basedir}/${YEAR} &>/dev/null
+create_graph -d1 -p $PAIR -i 4h -o ${basedir}/${YEAR}
 report ${base_dir}/${YEAR}/${PAIR}_${date}.xlsx &>> ${base_dir}/${YEAR}/${PAIR}_${date}.log
 redis-dump --db=1 > ${base_dir}/${YEAR}/${PAIR}_${date}.rs
