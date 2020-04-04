1aba82f4e415cef13b03fc0a008fd59e603a9c14
Amro Diab <adiab@hotmail.co.uk>
2020-04-04 22:45:54 +0100

    add margin balance to dashboard

--- dashboard/jobs/balance.rb
+++ dashboard/jobs/balance.rb
@@ -6,9 +6,13 @@ SCHEDULER.every '30s' do
   db = Mysql2::Client.new(:host => "mysql", :username => "greencandle", :password => "password", :port => 3306, :database => "greencandle" )
   sql = "select round(sum(perc)/(datediff(max(sell_time),(select min(buy_time) from profit))),2) as average from profit"
 
+  balance_spot_query = "select round(gbp,2) as gbp from balance where coin='TOTALS' and exchange_id=4 order by ctime desc limit 1"
+  balance_spot = db.query(balance_spot_query)
 
-  balance_sql = "select round(gbp,2) as gbp from balance where coin='TOTALS' order by ctime desc limit 1"
-  balance = db.query(balance_sql)
+  balance_margin_query = "select round(gbp,2) as gbp from balance where coin='TOTALS' and exchange_id=5 order by ctime desc limit 1"
+  balance_margin = db.query(balance_spot_query)
+  balance = balance_spot.to_f + balance_margin.to_f
+
   result = db.query(sql)
   last_balance = current_balance
   if balance.size == 0
