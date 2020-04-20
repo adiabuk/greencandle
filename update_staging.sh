0c4bbf95df32ecf4891f2ca97a6b0c885a721c00
Amro Diab <adiab@hotmail.co.uk>
2020-04-20 14:25:15 +0100

    typo

--- update_staging.sh
+++ update_staging.sh
@@ -9,6 +9,6 @@ else
   export TAG=$(python greencandle/version.py)
 fi
 docker-compose -f ./install/docker-compose_stag.yml pull
-docker-compose -f ./install/docker-composei_stag.yml up -d
+docker-compose -f ./install/docker-compose_stag.yml up -d
 
 docker system prune --volumes --all -f
