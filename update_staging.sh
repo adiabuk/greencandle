893b5d4fbc35ad52d665e0a89ed68a2c4bd6c735
Amro Diab <adiab@hotmail.co.uk>
2020-04-22 10:18:53 +0100

    stagger startup of strategy containers

--- update_staging.sh
+++ update_staging.sh
@@ -9,6 +9,17 @@ else
   export TAG=$(python greencandle/version.py)
 fi
 docker-compose -f ./install/docker-compose_stag.yml pull
-docker-compose -f ./install/docker-compose_stag.yml up -d
+base=$(yq r install/*stag* services | grep -v '^ .*' | sed 's/:.*$//'|grep -vE '\-|cron|api')
+gc=$(yq r install/*stag* services | grep -v '^ .*' | sed 's/:.*$//'|grep '\-')
+
+docker-compose -f ./install/docker-compose_stag.yml up -d $base
+
+for i in $gc; do
+  docker-compose -f ./install/docker-compose_stag.yml up -d $i
+  sleep 60
+done
+
+sleep 120
+docker-compose -f ./install/docker-compose_stag.yml up -d cron api
 
 docker system prune --volumes --all -f
