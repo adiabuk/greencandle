4e3487352cf10110950fb0c21fcfebb89cd3e3ee
Amro Diab <adiab@hotmail.co.uk>
2020-04-25 19:44:15 +0100

    missing interval in docker test  script

--- greencandle/scripts/run_in_docker.sh
+++ greencandle/scripts/run_in_docker.sh
@@ -17,7 +17,7 @@ mkdir -p ${base_dir}/${YEAR}
 echo $PAIR $date
 backend_test -d /data/altcoin_historical/${YEAR}/year/ -s -i $INTERVAL -p $PAIR &> ${base_dir}/${YEAR}/${PAIR}_${date}.log
 create_graph -d0 -p $PAIR -i $INTERVAL -o ${base_dir}/${YEAR}
-report ${base_dir}/${YEAR}/${PAIR}_${date}.xlsx &>> ${base_dir}/${YEAR}/${PAIR}_${date}.log
+report $INTERVAL ${base_dir}/${YEAR}/${PAIR}_${date}.xlsx &>> ${base_dir}/${YEAR}/${PAIR}_${date}.log
 redis-dump --db=0 > ${base_dir}/${YEAR}/${PAIR}_${date}.rs
 mysqldump --protocol=tcp -uroot -ppassword greencandle > ${base_dir}/${YEAR}/${PAIR}_${date}.sql
 gzip ${base_dir}/${YEAR}/${PAIR}_${date}.sql ${base_dir}/${YEAR}/${PAIR}_${date}.log
