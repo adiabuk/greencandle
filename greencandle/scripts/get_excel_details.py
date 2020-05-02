98d322ee2cf2153e77bbd3ece4d7d56388724808
Amro Diab <adiab@hotmail.co.uk>
2020-05-02 22:25:53 +0100

    get monthly results from excel

--- greencandle/scripts/get_excel_details.py
+++ greencandle/scripts/get_excel_details.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """
 Get total sum of perc profit from each xlsx file in current directory
+Monthly and Annually
 """
 
+import sys
 import glob
 import pandas as pd
 
@@ -13,10 +15,20 @@ def main():
     Main function
     """
     files = glob.glob('*.xlsx')
+    if len(sys.argv) != 2 or sys.argv[1] not in ("annual", "monthly"):
+        sys.stderr.write("Usage: {} <annual|monthly>\n".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        output = sys.argv[1]
+
     for file in files:
-        dframe = pd.read_excel(file, sheet_name='profit-pair')
-        print(dframe["pair"].to_string(header=False).split()[-1],
-              dframe["perc"].to_string(header=False).split()[-1])
+        if output == "annual":
+            dframe = pd.read_excel(file, sheet_name='profit-pair')
+            print(dframe["pair"].to_string(header=False).split()[-1],
+                  dframe["perc"].to_string(header=False).split()[-1])
+        elif output == "monthly":
+            dframe = pd.read_excel(file, sheet_name='monthly')
+            print(dframe)
 
 if __name__ == '__main__':
     main()
