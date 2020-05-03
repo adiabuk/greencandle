b146e2fd912fa2ee9cd90d1de1f24e240076928a
Amro Diab <adiab@hotmail.co.uk>
2020-05-03 02:21:00 +0100

    fix unit tests

--- greencandle/scripts/get_excel_details.py
+++ greencandle/scripts/get_excel_details.py
@@ -15,11 +15,15 @@ def main():
     Main function
     """
     files = glob.glob('*.xlsx')
-    if len(sys.argv) != 2 or sys.argv[1] not in ("annual", "monthly"):
+    if len(sys.argv) != 2 or sys.argv[1] not in ("annual", "monthly", "--help"):
         sys.stderr.write("Usage: {} <annual|monthly>\n".format(sys.argv[0]))
         sys.exit(1)
-    else:
-        output = sys.argv[1]
+    elif sys.argv[1] == '--help':
+        sys.stderr.write("Extract data from Excel reports\n")
+        sys.stderr.write("Usage: {} <annual|monthly>\n".format(sys.argv[0]))
+        sys.exit(0)
+
+    output = sys.argv[1]
 
     for file in files:
         if output == "annual":
