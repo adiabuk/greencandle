52cd02224cf23c3bb26eb6a29276ff72257b4c05
Amro Diab <adiab@hotmail.co.uk>
2020-05-17 03:15:25 +0100

    get profit factor from report

--- greencandle/scripts/get_excel_details.py
+++ greencandle/scripts/get_excel_details.py
@@ -15,8 +15,8 @@ def main():
     Main function
     """
     files = glob.glob('*.xlsx')
-    if len(sys.argv) != 2 or sys.argv[1] not in ("annual", "monthly", "--help"):
-        sys.stderr.write("Usage: {} <annual|monthly>\n".format(sys.argv[0]))
+    if len(sys.argv) != 2 or sys.argv[1] not in ("annual", "monthly", "factor", "--help"):
+        sys.stderr.write("Usage: {} <annual|monthly|factor>\n".format(sys.argv[0]))
         sys.exit(1)
     elif sys.argv[1] == '--help':
         sys.stderr.write("Extract data from Excel reports\n")
@@ -26,13 +26,20 @@ def main():
     output = sys.argv[1]
 
     for file in files:
-        if output == "annual":
-            dframe = pd.read_excel(file, sheet_name='profit-pair')
-            print(dframe["pair"].to_string(header=False).split()[-1],
-                  dframe["perc"].to_string(header=False).split()[-1])
-        elif output == "monthly":
-            dframe = pd.read_excel(file, sheet_name='monthly')
-            print(dframe)
+        try:
+            if output == "annual":
+                dframe = pd.read_excel(file, sheet_name='profit-pair')
+                print(dframe["pair"].to_string(header=False).split()[-1],
+                      dframe["perc"].to_string(header=False).split()[-1])
+            elif output == "monthly":
+                dframe = pd.read_excel(file, sheet_name='perc-month')
+                print(dframe)
+            elif output =="factor":
+                dframe = pd.read_excel(file, sheet_name='profit-factor')
+                print(file, dframe["profit_factor"].to_string(header=False).split()[-1])
+        except:
+            continue
+
 
 if __name__ == '__main__':
     main()
