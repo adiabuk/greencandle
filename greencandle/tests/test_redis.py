d2ce00480a53ea99dbc147835bc03910a62fc4b3
Amro Diab <adiab@hotmail.co.uk>
2020-12-13 22:08:25 +0000

    fix redis tests to reflect new changes

--- greencandle/tests/test_redis.py
+++ greencandle/tests/test_redis.py
@@ -2,6 +2,7 @@
 """Redis tests"""
 
 import unittest
+import random
 import time
 from greencandle.lib import config
 config.create_config()
@@ -44,22 +45,30 @@ class TestRedis(unittest.TestCase):
         """
 
         redis = Redis()
+        redis.clear_all()
         items = redis.get_items('ABCDEF', '10m')
         self.assertEqual(len(items), 0)
 
-        redis.redis_conn('ETHBTC', '1m', {"a":1}, time.time())
+        redis.redis_conn('ETHBTC', '1m', {"a":1},1607896214)
         items = redis.get_items('ABCDEF', '10m')
         self.assertEqual(len(items), 0)
 
+        items = redis.get_items('ETHBTC', '1m')
+        self.assertEqual(len(items), 0)
+
+        redis.redis_conn('ETHBTC', '1m', {"a":1}, 1609108999)
         items = redis.get_items('ETHBTC', '1m')
         self.assertEqual(len(items), 1)
 
-        redis.redis_conn('ETHBTC', '1m', {"a":1}, time.time())
-        items = redis.get_items('ETHBTC', '1m')
-        self.assertEqual(len(items), 2)
+        def random_epoch():
+            start_time = int(time.time())
+            end_time = start_time + 5184000    # 60 days
+            time_str=str(random.randint(start_time, end_time))
+            time_str.replace(" ", "").rstrip(time_str[-3:]).upper()
+            return int(time_str[:-3] +'999')
 
         for _ in range(10):
-            redis.redis_conn('ETHBNB', '1m', {"a":1}, time.time())
+            redis.redis_conn('ETHBNB', '1m', {"a":1}, random_epoch())
             time.sleep(1)
         items = redis.get_items('ETHBNB', '1m')
         self.assertEqual(len(items), 10)
