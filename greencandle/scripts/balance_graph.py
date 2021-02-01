a9be629e2d0c1121d678a076575794b3d6c1d179
Amro Diab <adiab@hotmail.co.uk>
2021-02-01 14:04:11 +0000

    complete balance cleanup and graphing script

--- greencandle/scripts/balance_graph.py
+++ greencandle/scripts/balance_graph.py
@@ -10,8 +10,8 @@ import numpy
 import pandas
 from greencandle.lib import config
 config.create_config()
-from greencandle.lib.mysql import Mysql
 
+from greencandle.lib.mysql import Mysql
 from greencandle.lib.graph import Graph
 
 
@@ -25,24 +25,42 @@ def main():
         print("Create OHLC graph using daily balance")
         sys.exit(0)
 
-    query = """
-    select
-           left(ctime,10) as closeTime,
-           (select usd group by left(ctime,10) order by ctime asc limit 1) as open,
-           max(usd) as high,
-           min(usd) as low,
-           (select usd group by left(ctime,10) order by ctime desc limit 1) as close
-            from(
-    select ctime, sum(usd) as usd from balance where coin='TOTALS' group by ctime
-    ) as t group by left(ctime,10);
-    """
+    # temp table we will use to filter out incomplete balance records
+    drop = "drop table if exists `temp_table`"
+
+    # There needs to be 3 records for each date representing 3 exchanges
+    clean = ("create table temp_table  as SELECT ctime FROM balance where coin='TOTALS' "
+             "GROUP BY ctime HAVING count(*) <3; delete from balance where ctime "
+             "in(select * from temp_table);")
+
+    # Update balance summary and clean out old balance records
+    update = ("insert into balance_summary (select ctime, sum(usd) as usd from balance where "
+              "coin='TOTALS' and left(ctime,10) != CURDATE() group by ctime); delete from "
+              "balance where left(ctime,10) != CURDATE();")
+
+    # Generate OHLC data
+    query = ("select c1.ctime as closeTime, (SELECT c2.usd FROM balance_summary c2 WHERE "
+             "c2.ctime = MIN(c1.ctime)) AS open, MAX(c1.usd) AS high, MIN(c1.usd) AS low, "
+             "(SELECT c2.usd FROM balance_summary c2 WHERE c2.ctime = MAX(c1.ctime)) "
+             "AS close FROM balance_summary c1  GROUP BY left(ctime,10) "
+             "ORDER BY c1.ctime ASC;")
+
+    # Run queries
+    mysql.fetch_sql_data(drop)
+    mysql.fetch_sql_data(clean)
+    mysql.fetch_sql_data(drop)
+    mysql.fetch_sql_data(update)
     results = mysql.fetch_sql_data(query)
+
+    # Convert results into pandas dataframe using header as column title
     dframe = pandas.DataFrame(results, columns=results.pop(0))
     dframe['closeTime'] = pandas.to_datetime(dframe['closeTime']).values.astype(numpy.int64) \
             // 10**6
+
+    # Generate graph
     graph = Graph(False, pair="Balance", interval='1d', volume=False)
     graph.insert_data(dframe)
-    graph.create_graph('.')
+    graph.create_graph('/data/graphs')
 
 if __name__ == '__main__':
     main()
