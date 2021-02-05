7dba65a6ce7a7a118b94a927d99433c165773bc1
Amro Diab <adiab@hotmail.co.uk>
2021-02-05 20:36:40 +0000

    more accurate perc diff

--- database_change.sql
+++ database_change.sql
@@ -1,5 +1,6 @@
--- Version 1.8
+-- Version 1.9
 
 use greencandle;
 
-drop table if exists symbols;
+drop view if exists profit;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `profit` AS select `trades`.`open_time` AS `open_time`,`trades`.`interval` AS `interval`,`trades`.`close_time` AS `close_time`,`trades`.`pair` AS `pair`,`trades`.`name` AS `name`,`trades`.`open_price` AS `open_price`,`trades`.`close_price` AS `close_price`,(case when (`trades`.`direction` = 'long') then (((`trades`.`close_price` - `trades`.`open_price`) / `trades`.`open_price`) * 100) else (((`trades`.`base_in` - `trades`.`base_out`) / `trades`.`base_in`) * 100) end) AS `perc`,(case when (`trades`.`direction` = 'long') then (`trades`.`base_out` - `trades`.`base_in`) else (`trades`.`base_in` - `trades`.`base_out`) end) AS `base_profit`,`trades`.`drawdown_perc` AS `drawdown_perc`,`trades`.`drawup_perc` AS `drawup_perc` from `trades`;
