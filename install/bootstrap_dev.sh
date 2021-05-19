c7c0bfe09d0200fd9a68792bc8da75932be04a11
Amro Diab <adiab@hotmail.co.uk>
2021-05-19 10:58:03 +0100

    replace missing pip package versions

--- install/bootstrap_dev.sh
+++ install/bootstrap_dev.sh
@@ -110,7 +110,7 @@ mkdir -p /data/{mysql,config,graphs,reports}
 install_dir=/srv/greencandle
 [[ ! -d $install_dir ]] && ln -s /home/travis/build/adiabuk/greencandle/ $install_dir
 cd $install_dir
-pip install pip==9.0.1 numpy==1.16.0 ccxt==1.38.76 cryptography==3.3.1
+pip install pip==9.0.1 numpy==1.16.0 ccxt==1.50.10 cryptography==3.3.1
 
 python ./setup.py install
 pip install pytest redis-dump-load gitpython setuptools==45.1.0
