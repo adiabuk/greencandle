f2c2dd942875b28a8d512a75d16f23983bda976c
Amro Diab <adiab@hotmail.co.uk>
2021-09-15 17:09:51 +0100

    db changes for next release

--- database_change.sql
+++ database_change.sql
@@ -1,3 +1,10 @@
--- Version 2.3.1
+-- Version 2.10
 
+
+ALTER TABLE trades ADD open_usd_rate varchar(30);
+ALTER TABLE trades ADD open_gbp_rate varchar(30);
+ALTER TABLE trades ADD close_usd_rate varchar(30);
+ALTER TABLE trades ADD close_usd_rate varchar(30);
+UPDATE trades SET close_usd_rate = rate;
+ALTER TABLE TABLE Trades DROP COLUMN rate;
 
