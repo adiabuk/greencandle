6cb0d4690d32210c2b76a41e92f75a97217cbce7
Amro Diab <adiab@hotmail.co.uk>
2021-09-20 20:01:23 +0100

    alert in slack if pair not in config

--- greencandle/lib/order.py
+++ greencandle/lib/order.py
@@ -121,7 +121,7 @@ class Trade():
             if current_trades and item[0] in current_trades[0]:
                 self.logger.warning("We already have a trade of %s, skipping..." % item[0])
             elif item[0] not in self.config.main.pairs and (not self.test_data or not manual):
-                self.logger.warning("Pair %s not in main_pairs, skipping..." % item[0])
+                self.logger.error("Pair %s not in main_pairs, skipping..." % item[0])
             else:
                 final_list.append(item)
         return final_list
