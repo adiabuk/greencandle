a8615186a9fe79adc662900cad1c2893c9fa737b
Amro Diab <adiab@linuxmail.org>
2021-11-16 18:32:15 +0000

    also edit backup trades table

--- database_change.sql
+++ database_change.sql
@@ -5,3 +5,10 @@ UPDATE trades
 
 UPDATE trades
    SET base_out=(@temp:=base_out), base_out = quote_out, quote_out = @temp
+
+
+UPDATE trades_16.01.21
+   SET base_in=(@temp:=base_in), base_in = quote_in, quote_in = @temp
+
+UPDATE trades_16.01.21
+   SET base_out=(@temp:=base_out), base_out = quote_out, quote_out = @temp
