ae0cda694c25d7870927c1a2f335c1b9745218dc
Amro Diab <adiab@linuxmail.org>
2021-11-16 18:18:20 +0000

    database changes

--- database_change.sql
+++ database_change.sql
@@ -1,2 +1,7 @@
--- Version 2.10
+-- Version 2.13
 
+UPDATE trades
+   SET base_in=(@temp:=base_in), base_in = quote_in, quote_in = @temp
+
+UPDATE trades
+   SET base_out=(@temp:=base_out), base_out = quote_out, quote_out = @temp
