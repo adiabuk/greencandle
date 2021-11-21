93f8696c73eb6864e8ec070967fec45d9dc24cc2
Amro Diab <adiab@hotmail.co.uk>
2021-11-21 16:36:07 +0000

    fix sql syntax

--- database_change.sql
+++ database_change.sql
@@ -1,17 +1,17 @@
 -- Version 2.14
 
 UPDATE trades
-   SET base_in=(@temp:=base_in), base_in = quote_in, quote_in = @temp
+   SET base_in=(@temp:=base_in), base_in = quote_in, quote_in = @temp;
 
 UPDATE trades
-   SET base_out=(@temp:=base_out), base_out = quote_out, quote_out = @temp
+   SET base_out=(@temp:=base_out), base_out = quote_out, quote_out = @temp;
 
 
 UPDATE trades_16.01.21
-   SET base_in=(@temp:=base_in), base_in = quote_in, quote_in = @temp
+   SET base_in=(@temp:=base_in), base_in = quote_in, quote_in = @temp;
 
 UPDATE trades_16.01.21
-   SET base_out=(@temp:=base_out), base_out = quote_out, quote_out = @temp
+   SET base_out=(@temp:=base_out), base_out = quote_out, quote_out = @temp;
 
 DROP view profit;
 CREATE VIEW `profit` AS select `trades`.`id` AS `id`,`trades`.`open_time` AS `open_time`,`trades`.`interval` AS `interval`,`trades`.`close_time` AS `close_time`, `trades`.`pair` AS `pair`,`trades`.`name` AS `name`,`trades`.`open_price` AS `open_price`,`trades`.`close_price` AS `close_price`,(case when (`trades`.`direction` = 'long') then (((`trades`.`quote_out` - `trades`.`quote_in`) / `trades`.`quote_in`) * 100) else (((`trades`.`open_price` - `trades`.`close_price`) / `trades`.`open_price`) * 100) end) AS `perc`,(case when (`trades`.`direction` = 'long') then (`trades`.`quote_out` - `trades`.`quote_in`) else (`trades`.`quote_in` - `trades`.`quote_out`) end) AS `quote_profit`,`trades`.`drawdown_perc` AS `drawdown_perc`,`trades`.`drawup_perc` AS `drawup_perc`,((`trades`.`quote_out` - `trades`.`quote_in`) * `trades`.`close_usd_rate`) AS `usd_profit` from `trades`;
@@ -27,4 +27,4 @@ DROP VIEW monthly_profit;
 CREATE VIEW `monthly_profit` AS select left(`profit`.`close_time`,7) AS `date`,`profit`.`interval` AS `interval`,sum(`profit`.`quote_profit`) AS `profit`,sum(`profit`.`perc`) AS `perc` from `profit` where (`profit`.`perc` is not null) group by left(`profit`.`close_time`,7) order by left(`profit`.`close_time`,7),sum(`profit`.`quote_profit`);
 
 DROP VIEW daily_profit;
-CREATE VIEW `daily_profit` AS select left(`profit`.`close_time`,10) AS `date`,sum(`profit`.`perc`) AS `perc`,sum(`profit`.`usd_profit`) AS `usd_profit` from `profit` where (`profit`.`perc` is not null) group by left(`profit`.`close_time`,10) order by left(`profit`.`close_time`,10),sum(`profit`.`quote_profit`)
+CREATE VIEW `daily_profit` AS select left(`profit`.`close_time`,10) AS `date`,sum(`profit`.`perc`) AS `perc`,sum(`profit`.`usd_profit`) AS `usd_profit` from `profit` where (`profit`.`perc` is not null) group by left(`profit`.`close_time`,10) order by left(`profit`.`close_time`,10),sum(`profit`.`quote_profit`);
