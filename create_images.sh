d6a94e09f49598827759cfe76a2b21ae72f4bb38
Amro Diab <adiab@hotmail.co.uk>
2021-11-25 13:45:38 +0000

    create alarm image

--- create_images.sh
+++ create_images.sh
@@ -10,14 +10,15 @@ else
   TAG="release-${TRAVIS_BRANCH}"
 fi
 
-# TODO: alarm image (as)
 # TODO: delete dashboard image? (ds)
 docker build -f install/Dockerfile-gc . -t amrox/greencandle:${TAG}
 docker build -f install/Dockerfile-ms . -t amrox/gc-mysql:${TAG}
 docker build -f install/Dockerfile-rs . -t amrox/gc-redis:${TAG}
 docker build -f install/Dockerfile-wb . -t amrox/webserver:${TAG}
+docker build -f install/Dockerfile-as . -t amrox/alert:${TAG}
 
 docker push amrox/greencandle:${TAG}
 docker push amrox/gc-mysql:${TAG}
 docker push amrox/gc-redis:${TAG}
 docker push amrox/webserver:${TAG}
+docker push amrox/alert:${TAG}
