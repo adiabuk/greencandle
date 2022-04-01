0338b6ee496bedc6d4ec6472b24527322187f201
Amro Diab <adiab@hotmail.co.uk>
2022-04-01 11:59:44 +0100

    compare sell amount with available balance

--- greencandle/bin/test_close.py
+++ greencandle/bin/test_close.py
@@ -12,6 +12,8 @@ from greencandle.lib.mysql import Mysql
 from greencandle.lib.auth import binance_auth
 from greencandle.lib.balance_common import get_step_precision
 from greencandle.lib.alerts import send_slack_message
+from greencandle.lib.balance_common import get_base
+from greencandle.lib.balance import Balance
 
 def main():
     """ Main function """
@@ -26,15 +28,27 @@ def main():
     query = ("select pair, base_in, name from trades where close_price is NULL")
 
     open_trades = dbase.fetch_sql_data(query, header=False)
-
+    bal = Balance(test=False)
+    balances = bal.get_balance()
     for trade in open_trades:
         pair, base_in, name = trade
         result = binance.spot_order(symbol=pair, side=binance.SELL,
                                     quantity=get_step_precision(pair, base_in),
                                     order_type=binance.MARKET, test=True)
         print("Testing {} from {}, result: {}".format(pair, name, str(result)))
+        print("comparing amount with available balance...")
+        try:
+            if 'isolated' in name:
+                bal_amount = balances['isolated'][pair][get_base(pair)]
+            elif 'cross' in name:
+                bal_amount = balances['margin'][get_base(pair)]['count']
+            else:
+                bal_amount = balances['binance'][get_base(pair)]['count']
+            result2 = float(base_in) < bal_amount
+        except KeyError:
+            result2 = True
 
-        if result:
+        if result or result2:
             send_slack_message("alerts", "Issue with open trade: {} {}".format(pair, name))
 
 if __name__ == '__main__':
