13bc585c4fbbef675c5f029961598c4108dab5d1
Amro Diab <adiab@hotmail.co.uk>
2022-04-12 20:44:22 +0100

    copy over greencandle.ini

--- install/docker-entrypoint-alert.sh
+++ install/docker-entrypoint-alert.sh
@@ -11,6 +11,7 @@ if [[ ! -e /installed ]]; then
   configstore package process_templates --ignore-role --basedir /opt/config $CONFIG_ENV /opt/output
   cp /opt/output/alert.ini /etc/alert.ini
   cp /srv/greencandle/alert/com.mp3 /
+  cp /opt/optput/greencandle.ini /etc
   touch /installed
 fi
 exec python "$@"
