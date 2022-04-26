5b7f1963c4e907e8fc9dcbc7a4bcd640833da328
Amro Diab <adiab@hotmail.co.uk>
2022-04-26 22:41:09 +0100

    get max borrowable for isolated balance

--- greencandle/bin/get_quote_balance.py
+++ greencandle/bin/get_quote_balance.py
@@ -8,6 +8,8 @@ from greencandle.lib.balance import Balance
 from greencandle.lib.auth import binance_auth
 from greencandle.lib.alerts import send_slack_message
 from greencandle.lib.common import QUOTES, arg_decorator, format_usd
+from greencandle.lib.balance_common import get_quote
+from greencandle.lib.binance_accounts import base2quote
 from greencandle.lib import config
 
 config.create_config()
@@ -51,11 +53,17 @@ def main():
     results += "\tBNB debt:{} {}\n".format(bnb_debt, format_usd(usd_debt))
     results += "Isolated Margin Account:\n"
     count = 0
-    for key, val in bal['isolated'].items():
-        if key != 'TOTALS' and float(val['count']) > 0:
-            results += "\t{} {} {}\n".format(quote, bal['isolated'][key]['count'],
-                                             format_usd(bal['isolated'][key]['USD']))
-            count += 1
+
+    for pair in bal['isolated'].keys():
+        if pair != 'TOTALS':
+            quote = get_quote(pair)
+            max_borrow = client.get_max_borrow(asset=quote, isolated_pair=pair)
+            usd = max_borrow if 'USD' in quote else base2quote(max_borrow, quote+'USDT')
+
+            if usd > 0:
+                results += "\tAvailable: " + format_usd(usd)+"\n"
+                count += 1
+
     if count == 0:
         results += "\tNone available"
     send_slack_message('balance', results)
