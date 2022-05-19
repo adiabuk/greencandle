316e4ac2e1e61f5de6ca936cde7206af61c7deaf
Amro Diab <adiab@hotmail.co.uk>
2022-05-19 21:53:31 +0100

    make time in redis match time submitted from engine

--- greencandle/lib/redis_conn.py
+++ greencandle/lib/redis_conn.py
@@ -214,7 +214,7 @@ class Redis():
         expiry = int(config.redis.redis_expiry_seconds)
 
         # closing time, 1 ms before next candle
-        if not key.endswith("99999"): # closing time, 1 ms before next candle
+        if not key.endswith("999"): # closing time, 1 ms before next candle
             self.logger.critical("Invalid time submitted to redis %s.  Skipping " % key)
             return None
 
