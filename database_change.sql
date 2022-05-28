f68f0ae90fad4676199ef7fd5125fb1f44d375e1
Amro Diab <adiab@hotmail.co.uk>
2022-05-28 18:44:03 +0100

    remove already applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,8 +1,2 @@
--- Version 3.6
-
-ALTER TABLE trades ADD COLUMN IF NOT EXISTS comm_open VARCHAR(255);
-ALTER TABLE trades ADD COLUMN IF NOT EXISTS comm_close VARCHAR(255);
-update trades set borrowed=0 where borrowed='';
-update trades set multiplier=0 where multiplier='';
-
+-- Version 3.7
 
