626634e0150c1bd7f282450b3bea0124fc103c81
Amro Diab <adiab@hotmail.co.uk>
2022-06-01 14:53:29 +0100

    fix rounding of perc

--- greencandle/lib/alerts.py
+++ greencandle/lib/alerts.py
@@ -92,7 +92,7 @@ def send_slack_trade(**kwargs):
         kwargs['price'] = str(kwargs['price']).rstrip("0")
         kwargs['perc'] = "%.4f" % (kwargs['perc'])
         commission = 0.2 if float(kwargs['perc']) > 0 else -0.2
-        kwargs['net_perc'] = str(float("%.4f" %  float(kwargs['perc'])) - float(commission)) + "%"
+        kwargs['net_perc'] = "%.4f" % float(float(kwargs['perc']) - float(commission)) + "%"
         kwargs['net_profit'] = format_usd(sub_perc(float(commission), float(kwargs['usd_profit'])))
         kwargs['usd_profit'] = format_usd(kwargs['usd_profit'])
         kwargs['perc'] = str(kwargs['perc']) + "%"
