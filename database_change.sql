6c4b2c99c5fac3919163ab3cdb486e059d72c599
Amro Diab <adiab@hotmail.co.uk>
2022-06-02 19:19:59 +0100

    remove applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,4 +1,1 @@
--- Version 3.8
-
-create view profitable_hours as select hour, sum(total_perc) as hour_perc, count(*) as total_count, sum((case when (`total_perc` > 0) then 1 else 0 end)) as num_profit,
-sum((case when (`total_perc` < 0) then 1 else 0 end)) as num_loss from hourly_profit group by hour;
+-- Version 3.9
