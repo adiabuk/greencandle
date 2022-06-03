1548c4a149b3afc3d4bd92da884d7e865c822804
Amro Diab <adiab@hotmail.co.uk>
2022-06-03 20:19:14 +0100

    fix and rename hourly profit script

--- greencandle/bin/get_hour_profit.py
+++ greencandle/bin/get_hour_profit.py
@@ -16,13 +16,14 @@ def main():
     mysql = Mysql()
     total_perc, avg_perc, usd_profit, hour = mysql.get_last_hour_profit()
     todays_perc = mysql.get_todays_profit()
-    message = ("Profit for Hour {0}\n"
-               "Total perc: {1:.2f}%\n"
-               "Average perc: {2:.2f}%\n"
-               "USD profit: {3}\n"
-               "Today's profit: {4:.2f}%".format(hour, total_perc, avg_perc,
-                                                 format_usd(usd_profit), todays_perc))
-    send_slack_message('balance', message)
+    if todays_perc:
+        message = ("Profit for Hour {0}\n"
+                   "Total perc: {1:.2f}%\n"
+                   "Average perc: {2:.2f}%\n"
+                   "USD profit: {3}\n"
+                   "Today's profit: {4:.2f}%".format(hour, total_perc, avg_perc,
+                                                     format_usd(usd_profit), todays_perc))
+        send_slack_message('balance', message)
 
 if __name__ == "__main__":
     main()
