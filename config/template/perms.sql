fea4b2789185480e3a70792602bf823323e6662c
Amro Diab <adiab@hotmail.co.uk>
2022-06-04 14:17:33 +0100

    no need to create root user

--- config/template/perms.sql
+++ config/template/perms.sql
@@ -1,6 +1,4 @@
-CREATE USER 'root'@'%' IDENTIFIED BY 'password';
 CREATE USER 'greencandle'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'greencandle'@'%' WITH GRANT OPTION;
 SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';
 SET GLOBAL max_connections = 1000;
