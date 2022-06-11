e542ddf022b42fdfff99c376c2bc85b5205499a8
Amro Diab <adiab@hotmail.co.uk>
2022-06-11 16:09:47 +0100

    test auth on logtailer

--- greencandle/bin/logtailer.py
+++ greencandle/bin/logtailer.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
-
+#pylint: disable=invalid-name
 """
 Flask module to provide web interface for tailing /var/log/syslog
 """
 
 import time
 from flask import Flask, render_template
+from flask_login import LoginManager, login_required
 from greencandle.lib.common import arg_decorator
+from greencandle.lib.flask_auth import load_user, login as loginx, logout as logoutx
 
 APP = Flask(__name__, template_folder="/etc/gcapi", static_url_path='/',
             static_folder='/etc/gcapi')
+LOGIN_MANAGER = LoginManager()
+LOGIN_MANAGER.init_app(APP)
+LOGIN_MANAGER.login_view = "login"
+APP.config['SECRET_KEY'] = 'shit'
+load_user = LOGIN_MANAGER.user_loader(load_user)
+login = APP.route("/login", methods=["GET", "POST"])(loginx)
+login = APP.route("/logout", methods=["GET", "POST"])(logoutx)
 
 @APP.route('/')
+@login_required
 def index():
     """default route"""
     return render_template('log.html')
 
 @APP.route('/stream')
+@login_required
 def stream():
     """
     stream path
