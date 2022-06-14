435b19fec166ccb2e4b6e6e65839804d527c1705
Amro Diab <adiab@hotmail.co.uk>
2022-06-14 18:04:43 +0100

    encrypt/decrypt creds for flask auth

---
+++ greencandle/bin/flask_auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#pylint: disable=no-member
+"""
+Module for encrypting and decrypting credentials with fernet lib
+"""
+
+import sys
+import getpass
+from cryptography.fernet import Fernet
+from greencandle.lib import config
+
+
+def main():
+    """
+    encrypt and decrypt credentials for flask api modules
+    format should take the form of "user:pass"
+    Usage: auth [encrypt|decrypt]
+    """
+    config.create_config()
+    key = config.web.auth_key
+    fernet = Fernet(key)
+
+    if sys.argv[1] == "encrypt":
+        username = input('username: ').strip()
+        password = getpass.getpass()
+        password2 = getpass.getpass("again: ")
+        if not password == password2:
+            print("Passwords don't match")
+            sys.exit(2)
+        message = "{}:{}".format(username, password)
+        encoded = fernet.encrypt(message.encode())
+        print(encoded)
+
+    elif sys.argv[1] == "decrypt":
+        auth_raw = config.web.flask_auth.encode()
+        user, passw = fernet.decrypt(auth_raw).decode().split(':')
+        #user, passw =  fernet.decrypt(auth).split(":")
+        print(user, passw)
+    else:
+        print("Invalid args")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
