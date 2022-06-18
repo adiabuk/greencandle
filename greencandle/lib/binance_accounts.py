de5d62f9cb302adbaea57ca372dccb3dd31b75ce
Amro Diab <adiab@hotmail.co.uk>
2022-06-18 11:22:20 +0100

    minor cleanup

--- greencandle/lib/binance_accounts.py
+++ greencandle/lib/binance_accounts.py
@@ -192,7 +192,7 @@ def get_binance_spot():
             if key == "BTC":
                 bcoin = float(current_value)
                 bitcoin_totals += float(bcoin)
-            elif key == "USDT" or key == "LDBUSD":  # LDBUSD is USD savings
+            elif key in ("USDT", "LDBUSD"):  # LDBUSD is USD savings
                 bcoin = float(current_value) / float(prices["BTCUSDT"])
                 bitcoin_totals += bcoin
             elif key == "GBP":
