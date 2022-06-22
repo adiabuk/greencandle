d4100f24b715ca9720c26eadd271aa805209c4ed
Amro Diab <adiab@hotmail.co.uk>
2022-06-22 19:57:11 +0100

    ensure usd quote amount is formatted for buy and sell notifs

--- greencandle/lib/alerts.py
+++ greencandle/lib/alerts.py
@@ -99,6 +99,7 @@ def send_slack_trade(**kwargs):
         kwargs['usd_profit'] = format_usd(kwargs['usd_profit'])
         kwargs['perc'] = str(kwargs['perc']) + "%"
         kwargs['quote'] = "%.4f" % (kwargs['quote'])
+        kwargs['usd_quote'] = format_usd(kwargs['usd_quote'])
 
     except TypeError:
         kwargs['net_perc'] = 'N/A'
