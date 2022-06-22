e430d91c7b06f95d19588360d1cb114424f3c64c
Amro Diab <adiab@hotmail.co.uk>
2022-06-22 23:25:06 +0100

    allow larger drawup/down and position together in table

--- database_change.sql
+++ database_change.sql
@@ -1,2 +1,5 @@
 -- Version 3.19
+ALTER TABLE trades MODIFY COLUMN drawdown_perc varchar(10);
+ALTER TABLE trades MODIFY COLUMN drawup_perc varchar(10);
+ALTER TABLE trades MODIFY drawup_perc varchar(10) AFTER drawdown_perc;
 
