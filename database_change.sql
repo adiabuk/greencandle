72e75216c76dfdd307a905796f6cbcee5333559b
Amro Diab <adiab@hotmail.co.uk>
2022-06-23 16:27:45 +0100

    clear applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,5 +1,2 @@
--- Version 3.19
-ALTER TABLE trades MODIFY COLUMN drawdown_perc varchar(10);
-ALTER TABLE trades MODIFY COLUMN drawup_perc varchar(10);
-ALTER TABLE trades MODIFY drawup_perc varchar(10) AFTER drawdown_perc;
+-- Version 3.21
 
