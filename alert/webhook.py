ce9650e202c1005c8584817e2e42c5ff6912b47a
Amro Diab <adiab@hotmail.co.uk>
2022-07-26 09:48:08 +0100

    increase alert volume

--- alert/webhook.py
+++ alert/webhook.py
@@ -110,7 +110,7 @@ def play_mp3(path):
     Play beep and downloaded synthesized audio
     Use 5% volume for out-of-hours alerts, otherwise 10% volume
     """
-    volume = '-g5' if get_time() == "night" else '-g15'
+    volume = '-g5' if get_time() == "night" else '-g50'
     subprocess.Popen(['mpg123', volume, '-q', path]).wait()
 
 def main():
