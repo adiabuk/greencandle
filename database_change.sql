303f9a206c8c9a1b019ce5bb9fc287996f8f2bb6
Amro Diab <adiab@hotmail.co.uk>
2022-07-28 16:16:06 +0100

    fix remove perc db func for negative amounts

--- database_change.sql
+++ database_change.sql
@@ -1,1 +1,5 @@
 -- Version 3.25
+
+drop function if exists REMOVE_PERCENT;
+CREATE FUNCTION `REMOVE_PERCENT`(amount decimal(10,2),perc decimal(10,2)) RETURNS decimal(10,2)
+RETURN IF(amount > 0, amount - (amount*perc/100), amount + (amount*perc/100));
