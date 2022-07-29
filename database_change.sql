00dd423ca3f5ffcafac3e063a81134eb118fe569
Amro Diab <adiab@hotmail.co.uk>
2022-07-29 14:23:16 +0100

    remove applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,5 +1,1 @@
--- Version 3.25
-
-drop function if exists REMOVE_PERCENT;
-CREATE FUNCTION `REMOVE_PERCENT`(amount decimal(10,2),perc decimal(10,2)) RETURNS decimal(10,2)
-RETURN IF(amount > 0, amount - (amount*perc/100), amount + (amount*perc/100));
+-- Version 3.26
