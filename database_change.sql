8704b39e3892353c36ae75262571495f6b559de6
Amro Diab <adiab@hotmail.co.uk>
2022-07-30 17:45:06 +0100

    remove applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,8 +1,1 @@
--- Version 3.26
-
-rename table if exists profitable_by_date to profitable_daily;
-
-drop view if exists profit_daily;
-drop table if exists profit_daily;
-create view profit_daily as
-select left(`profit`.`close_time`,10) AS `date`,dayname(`profit`.`close_time`) AS `day`,sum(`profit`.`perc`) AS `total_perc`,avg(`profit`.`perc`) AS `avg_perc`,sum(`profit`.`usd_profit`) AS `usd_profit`,count(0) AS `count` from `greencandle`.`profit` where `profit`.`perc` is not null group by left(`profit`.`close_time`,10) order by left(`profit`.`close_time`,10) desc;
+-- Version 3.27
