aa07548d3b4cfc5cfd8e9af03275041d3cd3996e
Amro Diab <adiab@hotmail.co.uk>
2022-08-17 00:19:14 +0100

    ensure we have one record for each exchange/time when creating balance graph

--- greencandle/bin/balance_graph.py
+++ greencandle/bin/balance_graph.py
@@ -23,39 +23,28 @@ def main():
     """
     mysql = Mysql()
 
-    # temp table we will use to filter out incomplete balance records
-    drop = "drop table if exists `temp_table`"
-
-    # There needs to be 4 records for each date representing 3 exchanges
-    clean = ("create table temp_table  as SELECT ctime FROM balance where coin='TOTALS' "
-             "GROUP BY ctime HAVING count(*) <4")
-
-    delete1 = "delete from balance where ctime in (select * from temp_table)"
-
     # Update balance summary and clean out old balance records
     update = ("insert into balance_summary (select ctime, sum(usd) as usd, sum(btc) as btc "
-              "from balance where coin='TOTALS' and left(ctime,10) != CURDATE() group by ctime)")
+              "from balance where coin='TOTALS' and date(ctime) != CURDATE() group by ctime "
+              "HAVING count(0)=(select count(0) from exchange))")
 
-    delete2 = "delete from balance where left(ctime,10) != CURDATE()"
+    # Delete old records
+    delete = "delete from balance where date(ctime) != CURDATE()"
 
     # Generate OHLC data
     query = ("select c1.ctime as openTime, (SELECT c2.{0} FROM balance_summary c2 WHERE "
              "c2.ctime = MIN(c1.ctime) limit 1) AS open, MAX(c1.{0}) AS high, MIN(c1.{0}) AS low, "
              "(SELECT c2.{0} FROM balance_summary c2 WHERE c2.ctime = MAX(c1.ctime) limit 1) "
-             "AS close FROM balance_summary c1  GROUP BY left(ctime,10) "
+             "AS close FROM balance_summary c1  GROUP BY date(ctime) "
              "ORDER BY c1.ctime ASC")
     btc_query = query.format("btc")
     usd_query = query.format("usd")
 
     # Run queries
-    mysql.run_sql_statement(drop)
-    mysql.run_sql_statement(clean)
-    mysql.run_sql_statement(delete1)
-    mysql.run_sql_statement(drop)
     mysql.run_sql_statement(update)
     usd_results = mysql.fetch_sql_data(usd_query)
     btc_results = mysql.fetch_sql_data(btc_query)
-    mysql.run_sql_statement(delete2)
+    mysql.run_sql_statement(delete)
 
     # Convert results into pandas dataframe using header as column title
     usd_dframe = pandas.DataFrame(usd_results, columns=usd_results.pop(0))
