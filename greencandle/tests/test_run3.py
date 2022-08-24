f4cd330a20fdd761eac7f225697e4a0007d7b7bd
Amro Diab <adiab@hotmail.co.uk>
2022-08-24 21:43:39 +0100

    add margin long&short tests

--- greencandle/tests/test_run3.py
+++ greencandle/tests/test_run3.py
@@ -2,12 +2,13 @@
 
 """
 Unittest file for testing results of a run using downloaded data
+Rules for short have not been revered from long
 """
 import unittest
 from .unittests import make_test_case
 
-class TestBNBETH(make_test_case('unit/cross', 'BNBETH', '1h', '2019-02-27', 15, 35, 14, -5.5,
-                                61, 9.2)):
+class TestBNBETHCrossLong(make_test_case('unit/cross', 'BNBETH', '1h', '2019-02-27',
+                                          15, 35, 14, -5.5, 61, 9.2)):
     """
     Test BNBETH
     cross margin long divisor=2
@@ -15,14 +16,34 @@ class TestBNBETH(make_test_case('unit/cross', 'BNBETH', '1h', '2019-02-27', 15, 
 
     pass
 
-class TestBNBETH(make_test_case('unit/isolated-long', 'BNBETH', '1h', '2019-02-27', 15, 35, 14, -5.5,
-                                61, 9.2)):
+class TestBNBETHIsoLong(make_test_case('unit/isolated-long', 'BNBETH', '1h', '2019-02-27',
+                                       15, 35, 14, -5.5, 61, 9.2)):
     """
     Test BNBETH
-    cross margin long divisor=2
+    isolated margin long divisor=2
     """
 
     pass
+
+class TestBNBETHIsoShort(make_test_case('unit/isolated-short', 'BNBETH', '1h',
+                                        '2019-02-27', 15, -46, 3, -21, 13.8, 9.2)):
+    """
+    Test BNBETH
+    isolated margin short divisor=2
+    """
+
+    pass
+
+
+class TestBNBETHCrossShort(make_test_case('unit/cross-short', 'BNBETH', '1h',
+                                          '2019-02-27', 15, -46, 3, -21, 13.8, 9.2)):
+    """
+    Test BNBETH
+    cross margin short divisor=2
+    """
+
+    pass
+
 
 
 if __name__ == '__main__':
