aa34951ad100c45cbf3f07df899fbff82f72f47e
Amro Diab <adiab@hotmail.co.uk>
2022-08-24 17:42:14 +0100

    add isolated long test

--- greencandle/tests/test_run3.py
+++ greencandle/tests/test_run3.py
@@ -15,5 +15,15 @@ class TestBNBETH(make_test_case('unit/cross', 'BNBETH', '1h', '2019-02-27', 15, 
 
     pass
 
+class TestBNBETH(make_test_case('unit/isolated-long', 'BNBETH', '1h', '2019-02-27', 15, 35, 14, -5.5,
+                                61, 9.2)):
+    """
+    Test BNBETH
+    cross margin long divisor=2
+    """
+
+    pass
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=6)
