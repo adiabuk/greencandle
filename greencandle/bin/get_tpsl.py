b6cadbf932fec8fa6f7515343180c9b11d3ae264
Amro Diab <adiab@hotmail.co.uk>
2022-09-17 12:24:24 +0100

    get tpsl from redis

---
+++ greencandle/bin/get_tpsl.py
@@ -0,0 +1,27 @@
+#pylint: disable=no-member
+"""
+Retrieve SL and TP from redis
+"""
+import sys
+from greencandle.lib.common import arg_decorator
+from greencandle.lib.redis_conn import Redis
+from greencandle.lib import config
+
+
+@arg_decorator
+def main():
+    """
+    Retrieve TP and SL from redis for given pair
+    Usage: get_tpsl <pair> 
+    """
+
+    config.create_config()
+    redis = Redis()
+    pair = sys.argv[1]
+    take_profit = redis.get_on_entry(pair, 'take_profit_perc')
+    stop_loss = redis.get_on_entry(pair, 'stop_loss_perc')
+    
+    print("TP: {}\nSL: {}".format(take_profit, stop_loss))
+
+if __name__ == '__main__':
+    main()
