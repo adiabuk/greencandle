bce31de4cb770b5eb64ba15b1e1aee9f874439c5
Amro Diab <adiab@hotmail.co.uk>
2022-09-22 22:31:40 +0100

    reduce log clutter

--- greencandle/lib/redis_conn.py
+++ greencandle/lib/redis_conn.py
@@ -322,7 +322,7 @@ class Redis():
                                      format(float(kwargs.perc_rate), ".4f"),
                                      kwargs.open_price, kwargs.close_price, kwargs.current_time))
 
-        if kwargs.event == "HOLD":
+        if kwargs.event == "HOLD" or kwargs.event == "NOITEM":
             self.logger.debug("%s, %s" % (message, kwargs.current))
         else:
             self.logger.info("%s, %s" % (message, kwargs.current))
