bb90a6f6a0fc07cb10cf3982c29031c1fba2d07a
Amro Diab <adiab@hotmail.co.uk>
2022-09-26 15:43:38 +0100

    test for exceeding borrow limit

--- greencandle/tests/test_borrowed.py
+++ greencandle/tests/test_borrowed.py
@@ -45,6 +45,31 @@ class TestBorrow(unittest.TestCase):
         self.assertEqual(config.main.trade_direction, "long")
         self.assertEqual(amt, sub_perc(1, (5000+20)/3))
 
+    def test_cross_long_exceed(self):
+        """
+        cross long with 2 shorts open and exceeding max borrowable amt
+        """
+        os.system("configstore package process_templates unit/borrow/cross /etc")
+
+        mock = Mock()
+        value = list((('WAVESUSDT', '1', 'short'), ('BCHUSDT', '1', 'short')))
+        instance = mock.return_value
+        instance.get_current_borrowed.return_value = value
+
+        client = mock.return_value
+        client.get_max_borrow.return_value = 5000
+        pair = "BTCUSDT"
+        with patch('greencandle.lib.order.base2quote', return_value=2000): #2000 USD per item
+
+            trade = Trade(interval='15m', test_data=False, test_trade=False, config=config)
+            trade.client = client
+            amt = trade.get_amount_to_borrow(pair, instance)
+
+        self.assertEqual(config.main.trade_direction, "long")
+        # Same as previous test, except we shouldn't have the same value due to max_borrow being exceeded
+        self.assertNotEqual(amt, sub_perc(1, (5000+20)/3))
+
+
     def test_cross_long2(self):
         """
         Change existing trades from short to long
