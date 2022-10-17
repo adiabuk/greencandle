58f170bfff74a674d0e632c9fd193a5f8e380619
Amro Diab <adiab@hotmail.co.uk>
2022-10-17 17:50:55 +0100

    fix assocs

--- greencandle/tests/test_assocs.py
+++ greencandle/tests/test_assocs.py
@@ -18,7 +18,7 @@ class TestAssocs(unittest.TestCase):
         Ensure all envs in router configs exist
         """
 
-        envs = (('per', 'PER'), ('prod', 'PROD'), ('stag', 'STAG'))
+        envs = (('per', 'PER'), ('prod', 'PROD'), ('stag', 'STAG'), ('test', 'TEST'))
         for env, host in envs:
             os.system("sudo configstore package process_templates {} /tmp".format(env))
             with open('/tmp/router_config.json', 'r') as json_file:
@@ -29,7 +29,7 @@ class TestAssocs(unittest.TestCase):
                     output = (yaml.safe_load(stream))
                 except yaml.YAMLError as exc:
                     print(exc)
-            print("Testing " + env)
+            print("Testing", env)
             links_list = output['services']['{}-be-api-router'.format(env)]['links']
             service_list = [item.split(":")[-1] for item in links_list]
 
@@ -37,16 +37,17 @@ class TestAssocs(unittest.TestCase):
             router_config_flat_list = [item for sublist in router_config for item in sublist]
 
             for item in router_config_flat_list:
-                if env != 'stag' and item != 'alert':
-                    # alert uses hosts file in non-stag envs
+                if env != 'test' and item != 'alert':  # alert container only in test env
+                    # alert uses hosts file in non-test envs
                     self.assertIn(item, service_list)
+        print("Done testing", env)
 
     @staticmethod
     def test_assocs():
         """
         Scrape environments from CONFIG_ENV vars in docker compose file and test
         """
-        envs = (('per', 'PROD'), ('prod', 'PROD'), ('stag', 'STAG'))
+        envs = (('per', 'PER'), ('prod', 'PROD'), ('test', 'TEST'), ('stag', 'STAG'))
         for env, host in envs:
             print("processing env {}".format(env))
             os.system("sudo configstore package process_templates {} /tmp".format(env))
@@ -62,15 +63,21 @@ class TestAssocs(unittest.TestCase):
                     output = (yaml.safe_load(stream))
                 except yaml.YAMLError as exc:
                     print(exc)
+            links_list = output['services']['{}-be-api-router'.format(env)]['links']
+            links_dict = list_to_dict(links_list)
+            for _, short_name in router_config.items():
+                for item in short_name:
+                    name = item.split(':')[0]
+                    if env != 'test' and name == 'alert':
+                        continue
+                    try:
+                        container = links_dict[name]
+                    except KeyError:
+                        print("Issue with {} in {}".format(name, env))
+                        raise
 
-        links_list = output['services']['{}-be-api-router'.format(env)]['links']
-        links_dict = list_to_dict(links_list)
-        for _, short_name in router_config.items():
-            for item in short_name:
-                name = item.split(':')[0]
-                container = links_dict[name]
-                if container.startswith('{}-be-'.format(env)) and 'alert' not in container:
-                    actual_name = container.replace('-be', '')  # strip off container type
-                else:
-                    continue
-                _ = rev_names[actual_name]
+                    if container.startswith('{}-be-'.format(env)) and 'alert' not in container:
+                        actual_name = container.replace('-be', '')  # strip off container type
+                    else:
+                        continue
+                    _ = rev_names[actual_name]
