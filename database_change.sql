2d2160a021d54a1a1e5092fc7222ce244d5ded6a
Amro Diab <adiab@hotmail.co.uk>
2022-10-18 08:22:02 +0100

    db changes for stag/test flip

--- database_change.sql
+++ database_change.sql
@@ -1,1 +1,12 @@
 # 4.8
+
+# for test:
+update trades set name='test-be-eng-bbpercst-short' where name='test-be-eng-bbperc-short';
+update trades set name='test-be-eng-bbpercst-long' where name='test-be-eng-bbperc-long';
+UPDATE trades SET name = REPLACE(field, 'test', 'stag') WHERE INSTR(field, 'test') > 0;
+
+# for stag:
+update trades set name='any-test-short' where name='test-any-short';
+update trades set name='any-test-long' where name='test-any-long';
+update trades set name='test-be-eng-bbpercorig-short' where name='test-be-eng-bbperc-short';
+update trades set name='test-be-eng-bbpercorig-long' where name='test-be-eng-bbperc-long';
