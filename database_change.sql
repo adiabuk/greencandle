ae42f624c973b70d2bb844651130e43ccb76ca83
Amro Diab <adiab@hotmail.co.uk>
2022-10-23 22:58:08 +0100

    remove applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,21 +1,2 @@
-# 4.8
+# 4.9
 
-# for test:
-update trades set name='test-be-eng-bbpercst-short' where name='test-be-eng-bbperc-short';
-update trades set name='test-be-eng-bbpercst-long' where name='test-be-eng-bbperc-long';
-update open_trades set name='test-be-eng-bbpercst-short' where name='test-be-eng-bbperc-short';
-update open_trades set name='test-be-eng-bbpercst-long' where name='test-be-eng-bbperc-long';
-UPDATE trades SET name = REPLACE(field, 'test', 'stag') WHERE INSTR(field, 'test') > 0;
-UPDATE open_trades SET name = REPLACE(field, 'test', 'stag') WHERE INSTR(field, 'test') > 0;
-
-# for stag:
-update trades set name='any-test-short' where name='test-any-short';
-update trades set name='any-test-long' where name='test-any-long';
-update open_trades set name='any-test-short' where name='test-any-short';
-update open_trades set name='any-test-long' where name='test-any-long';
-update trades set name='stag-be-eng-bbpercorig-short' where name='stag-be-eng-bbperc-short';
-update trades set name='stag-be-eng-bbpercorig-long' where name='stag-be-eng-bbperc-long';
-update open_trades set name='stag-be-eng-bbpercorig-short' where name='stag-be-eng-bbperc-short';
-update open_trades set name='stag-be-eng-bbpercorig-long' where name='stag-be-eng-bbperc-long';
-
-SHOW TABLE STATUS LIKE 'trades';
