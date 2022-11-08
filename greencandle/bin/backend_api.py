4d36d015de90405539e9d65b0c91dfade560ef4a
Amro Diab <adiab@hotmail.co.uk>
2022-11-08 13:49:22 +0000

    restore prod int check to api

--- greencandle/bin/backend_api.py
+++ greencandle/bin/backend_api.py
@@ -68,21 +68,22 @@ def main():
 
     Usage: backend_api
     """
-    scheduler = BackgroundScheduler()
-    #scheduler.add_job(func=consume_queue)
 
     #if "intermittent" in os.environ:
     #    scheduler.add_job(func=intermittent_check, trigger="interval", seconds=30)
 
-    #scheduler.start()
 
     if sys.argv[2] == 'api':
         APP.run(debug=False, host='0.0.0.0', port=20000, threaded=True)
+        if "intermittent" in os.environ:
+            scheduler = BackgroundScheduler()
+            scheduler.add_job(func=intermittent_check, trigger="interval", seconds=30)
+            scheduler.start()
     else:
         consume_queue()
 
 
-    if "intermittent" in os.environ:
+    if "intermittent" in os.environ and sys.argv[2] == 'api':
         # Shut down the scheduler when exiting the app
         atexit.register(scheduler.shutdown)
 
