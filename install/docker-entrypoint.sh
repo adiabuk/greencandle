7bed1c1b06b583d5380c8e7c43fcea3500c769d3
Amro Diab <adiab@hotmail.co.uk>
2022-11-08 21:32:37 +0000

    always re-process config after restart

--- install/docker-entrypoint.sh
+++ install/docker-entrypoint.sh
@@ -7,45 +7,42 @@ if [[ -z $CONFIG_ENV ]]; then
   exit 1
 fi
 
-if [[ ! -e /installed ]]; then
-  configstore package process_templates --ignore-role --basedir /opt/config $CONFIG_ENV /opt/output
-  cp /opt/output/greencandle.ini /opt/output/router_config.json /opt/output/alert.ini /etc
+configstore package process_templates --ignore-role --basedir /opt/config $CONFIG_ENV /opt/output
+cp /opt/output/greencandle.ini /opt/output/router_config.json /opt/output/alert.ini /etc
 
-  if [[ "$HOSTNAME" == *"web"* ]]; then
-    cp /opt/output/default.conf /etc/nginx/conf.d/default.conf
-    if [[ "$HOSTNAME" == *"webserver"* ]]; then
-      cp /opt/output/nginx.conf /etc/nginx/
-    else
-      cp /opt/output/default.ap /etc/nginx/conf.d/default.conf
-    fi
-    mkdir -p /var/www/html
-    echo $(configstore package get $CONFIG_ENV base_env --basedir /opt/config) > /var/www/html/env.txt
-    cp /opt/output/{*.html,*.css,*.js,*.jpg} /var/www/html
-    cp /opt/config/raw/main.css /opt/config/raw/50x.html /opt/config/raw/favicon.ico /var/www/html
-    > /etc/nginx/sites-available/default
+if [[ "$HOSTNAME" == *"web"* ]]; then
+  cp /opt/output/default.conf /etc/nginx/conf.d/default.conf
+  if [[ "$HOSTNAME" == *"webserver"* ]]; then
+    cp /opt/output/nginx.conf /etc/nginx/
+  else
+    cp /opt/output/default.ap /etc/nginx/conf.d/default.conf
+  fi
+  mkdir -p /var/www/html
+  echo $(configstore package get $CONFIG_ENV base_env --basedir /opt/config) > /var/www/html/env.txt
+  cp /opt/output/{*.html,*.css,*.js,*.jpg} /var/www/html
+  cp /opt/config/raw/main.css /opt/config/raw/50x.html /opt/config/raw/favicon.ico /var/www/html
+  > /etc/nginx/sites-available/default
 
-  elif [[ "$HOSTNAME" == *"api"* ]]; then
-    mkdir -p /etc/gcapi /var/www/html
-    echo $(configstore package get $CONFIG_ENV base_env --basedir /opt/config) > /var/www/html/env.txt
-    cp /opt/config/raw/* /etc/gcapi/
-    if [[ "$HOSTNAME" == *"long"* || "$HOSTNAME" == *"short"* ]]; then
-      command="backend_api $@ api & backend_api $@ queue"
-    fi
-
-
-  elif [[ "$HOSTNAME" == *"cron"* ]]; then
-    crontab /opt/output/gc-cron
-
-  elif [[ "$HOSTNAME" == *"alert"* ]]; then
-    cp /srv/alert/com.mp3 /srv/alert/250ms-silence.mp3 /
-
-  elif [[ "$HOSTNAME" == *"manual"* ]]; then
-    easy_install pip
-    pip3 install --upgrade pip setuptools ipython
+elif [[ "$HOSTNAME" == *"api"* ]]; then
+  mkdir -p /etc/gcapi /var/www/html
+  echo $(configstore package get $CONFIG_ENV base_env --basedir /opt/config) > /var/www/html/env.txt
+  cp /opt/config/raw/* /etc/gcapi/
+  if [[ "$HOSTNAME" == *"long"* || "$HOSTNAME" == *"short"* ]]; then
+    command="backend_api $@ api & backend_api $@ queue"
   fi
 
-  touch /installed
+
+elif [[ "$HOSTNAME" == *"cron"* ]]; then
+  crontab /opt/output/gc-cron
+
+elif [[ "$HOSTNAME" == *"alert"* ]]; then
+  cp /srv/alert/com.mp3 /srv/alert/250ms-silence.mp3 /
+
+elif [[ "$HOSTNAME" == *"manual"* ]]; then
+  easy_install pip
+  pip3 install --upgrade pip setuptools ipython
 fi
+
 
 if [[ $DB == true ]]; then
   mysql=$(awk -F "=" '/db_host/ {print $2}' /etc/greencandle.ini)
