fd3a2a089a24a3b930bf87c9fb7703c8afe6a3fd
Amro Diab <adiab@hotmail.co.uk>
2022-11-14 13:26:13 +0000

    only keep 250 items in redis

--- greencandle/bin/cleanup_redis.py
+++ greencandle/bin/cleanup_redis.py
@@ -5,43 +5,31 @@
 Redis cleanup-script
 """
 
-import datetime
 from greencandle.lib.redis_conn import Redis
-from greencandle.lib.common import arg_decorator, epoch2date
+from greencandle.lib.common import arg_decorator
 from greencandle.lib import config
 
 @arg_decorator
 def main():
     """
-    Cleanup old data from redis by removing candles > x days old
-    depending on interval
+    Cleanup old data from redis by all but the last 250 timeframes
+    for all intervals
     Run through cron, using pairs in config
     """
 
     pairs = config.main.pairs.split()
 
-    data = {'5m': 1,
-            '1h': 10,
-            '4h': 50
-            }
-
     redis = Redis()
     config.create_config()
     count = 0
-    for interval, days in data.items():
+    for interval in ['1m', '3m', '5m', '15m', '30m', '1h', '4h', '1d']:
         for pair in pairs:
             pair = pair.strip()
-            print("Analysing pair {}".format(pair))
+            print("Analysing pair {}:{}".format(pair, interval))
             items = redis.get_items(pair, interval)
-            max_date = datetime.datetime.today() - datetime.timedelta(days=days)
-            for item in items:
-                epoch = float(item)/1000.0
-                dtime = epoch2date(epoch, formatted=False)
-
-                if dtime < max_date:
-                    print("deleting {}".format(item))
-                    count += 1
-                    redis.conn.hdel("{}:{}".format(pair, interval), item)
+            for item in items[:-250]:
+                redis.conn.hdel('{}:{}'.format(pair, interval), item)
+                count += 1
     print("Deleted {} keys".format(count))
 
 
