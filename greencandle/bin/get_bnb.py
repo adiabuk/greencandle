086ed07d372df6c5fcbc3a3baf465fc586d33242
Amro Diab <adiab@hotmail.co.uk>
2022-11-20 21:52:27 +0000

    alert when not enough bnb in x-margin

---
+++ greencandle/bin/get_bnb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#pylint: disable=wrong-import-position,no-member
+
+"""
+Slack alert when not enough available BNB
+"""
+
+import sys
+from greencandle.lib.common import arg_decorator
+from greencandle.lib.balance import Balance
+from greencandle.lib.alerts import send_slack_message
+
+@arg_decorator
+def main():
+    """
+    Slack alert when not enough available BNB
+    Usage: get_bnb
+    """
+
+    balance = Balance(test=False)
+    all_bal = balance.get_balance()
+    bnb = float(all_bal['margin']['BNB']['USD'])
+    if bnb < 10:
+        error_str = "Not enough BNB in cross margin account"
+        send_slack_message('alerts', error_str, name=sys.argv[0].split('/')[-1])
+
+
+if __name__ == "__main__":
+    main()
