16fb73e8a200ac38ae3e5e02fa382fdd27f396d5
Amro Diab <adiab@hotmail.co.uk>
2022-11-20 22:30:37 +0000

    remove applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,5 +1,1 @@
-# 5.3
-
-drop function if exists get_var;
-CREATE FUNCTION `get_var`(`name_in` VARCHAR(30)) RETURNS varchar(30) CHARSET utf8mb4
-RETURN (select value from variables where `name`=name_in LIMIT 1)
+# 5.4
