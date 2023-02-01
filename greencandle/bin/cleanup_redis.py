37cf23f4b3d102dceabaab78fac85ab632d2936d
Amro Diab <adiab@hotmail.co.uk>
2023-02-01 11:00:32 +0000

    use large no of klines for redis cleanup and add logging

--- greencandle/bin/cleanup_redis.py
+++ greencandle/bin/cleanup_redis.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#pylint: disable=no-member
+#pylint: disable=no-member, logging-not-lazy
 
 """
 Redis cleanup-script
@@ -7,6 +7,7 @@ Redis cleanup-script
 
 import re
 from greencandle.lib.redis_conn import Redis
+from greencandle.lib.logger import get_logger
 from greencandle.lib.common import arg_decorator
 from greencandle.lib import config
 
@@ -17,13 +18,13 @@ def main():
     for all intervals
     Run through cron, using pairs available in redis
     """
-
+    logger = get_logger("cleanup_redis")
     redis = Redis()
     pairs = set()
     # Get unique set of uppercase pairs from redis keys
     for key in redis.conn.keys():
         if re.match('([A-Z]+):.*h', key.decode()):
-            pairs.add(re.findall('([A-Z]+):.*hm', key.decode())[0])
+            pairs.add(re.findall('([A-Z]+):.*[hm]', key.decode())[0])
     # convert set to list
     pairs = list(pairs)
 
@@ -32,12 +33,12 @@ def main():
     for interval in ['1m', '3m', '5m', '15m', '30m', '1h', '4h', '1d']:
         for pair in pairs:
             pair = pair.strip()
-            print("Analysing pair {}:{}".format(pair, interval))
+            logger.info("Analysing pair %s:%s" %(pair, interval))
             items = redis.get_items(pair, interval)
-            for item in items[:-250]:
+            for item in items[:-500]:
                 redis.conn.hdel('{}:{}'.format(pair, interval), item)
                 count += 1
-    print("Deleted {} keys".format(count))
+    logger.info("Deleted %s keys" % count)
 
 
 if __name__ == '__main__':
