09a8c0bcee37b14f92a49c6aa55b0b47618f94b2
Amro Diab <adiab@hotmail.co.uk>
2023-04-08 23:20:05 +0100

    timeframe for open graphs

--- greencandle/bin/open_graphs.sh
+++ greencandle/bin/open_graphs.sh
@@ -1,5 +1,20 @@
-for pair in `configstore package get data/5m pairs`; do
+#!/usr/bin/env bash
+
+declare -A TF2MIN
+
+TF2MIN[1h]="60"
+TF2MIN[4h]="240"
+TF2MIN[12h]="720"
+TF2MIN[1m]="1"
+TF2MIN[5m]="5"
+TF2MIN[15m]="15"
+TF2MIN[30m]="30"
+
+TF=$1
+MIN=${TF2MIN[$TF]}
+
+for pair in `configstore package get data/$TF pairs`; do
   echo $pair;
-  open "https://tradingview.com/chart?symbol=BINANCE:${pair}&interval=";
+  open "https://tradingview.com/chart?symbol=BINANCE:${pair}&interval=$MIN";
   read;
 done
