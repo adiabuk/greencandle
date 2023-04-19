93694c9293667f3d57ba7975dfcb488e9d5f2021
Amro Diab <adiab@hotmail.co.uk>
2023-04-19 16:15:50 +0100

    get open pairs in current scope

---
+++ greencandle/bin/get_scope_trades.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#pylint: disable=no-member
+"""
+Small command line tool to show pairs of open trades in current scope
+"""
+from greencandle.lib.mysql import Mysql
+from greencandle.lib.common import arg_decorator
+from greencandle.lib import config
+
+
+@arg_decorator
+def main():
+    """
+    Small command line tool to show pairs of open trades in current scope
+    """
+    dbase = Mysql()
+    config.create_config()
+    results = dbase.fetch_sql_data('select pair from open_trades where name="{}" and '
+                                   'direction="{}"'.format(config.main.name,
+                                                           config.main.trade_direction),
+                                   header=False)
+
+    pairs = [item for sublist in results for item in sublist]
+    for pair in pairs:
+        print(pair.strip())
+
+if __name__ == '__main__':
+    main()
