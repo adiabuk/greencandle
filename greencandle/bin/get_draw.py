f9a9ee635e61a0ee2770cd2b890eda1dad901e1d
Amro Diab <adiab@hotmail.co.uk>
2023-05-02 09:41:14 +0100

    check open trade when fetching draw up/down

--- greencandle/bin/get_draw.py
+++ greencandle/bin/get_draw.py
@@ -5,6 +5,7 @@ Retrieve SL and TP from redis
 import sys
 from greencandle.lib.common import arg_decorator
 from greencandle.lib.redis_conn import Redis
+from greencandle.lib.mysql import Mysql
 
 @arg_decorator
 def main():
@@ -12,6 +13,8 @@ def main():
     Retrieve TP and SL from redis for given pair
     Usage: get_draw <pair> <interval> <name> up|down <direction>
     """
+
+    dbase = Mysql()
     interval = sys.argv
     pair = sys.argv[1]
     interval = sys.argv[2]
@@ -19,16 +22,19 @@ def main():
     updown = sys.argv[4]
     direction = sys.argv[5]
 
-    redis = Redis(interval=interval, test_data=False, db=2)
+    if dbase.trade_in_context(pair, name, direction):
+        redis = Redis(interval=interval, test_data=False, db=2)
 
-    if updown == 'up':
-        result = redis.get_drawup(pair, name=name, direction=direction)
-    elif updown == 'down':
-        result = redis.get_drawdown(pair, name=name, direction=direction)
+        if updown == 'up':
+            result = redis.get_drawup(pair, name=name, direction=direction)
+        elif updown == 'down':
+            result = redis.get_drawdown(pair, name=name, direction=direction)
+        else:
+            result = 'error'
+
+        print(result)
     else:
-        result = 'error'
-
-    print(result)
+        print("No open trades in current context")
 
 if __name__ == '__main__':
     main()
