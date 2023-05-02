c2bb56e9e1e63fc6827661826b4f304da67665a8
Amro Diab <adiab@hotmail.co.uk>
2023-05-02 13:46:53 +0100

    check trade in context before running intermittent checks

--- greencandle/lib/run.py
+++ greencandle/lib/run.py
@@ -250,30 +250,32 @@ class ProdRunner():
 
             redis.update_drawdown(pair, current_candle, open_time=open_time)
             redis.update_drawup(pair, current_candle, open_time=open_time)
-            result, event, current_time, current_price = redis.get_intermittent(pair,
-                                                                                open_price,
-                                                                                current_candle,
-                                                                                open_time)
+            if dbase.trade_in_context(pair, config.main.name, config.main.trade_direction):
+                result, event, current_time, current_price = redis.get_intermittent(pair,
+                                                                                    open_price,
+                                                                                    current_candle,
+                                                                                    open_time)
 
-            LOGGER.debug("%s int check result: %s Open:%s Current:%s Time:%s"
-                         % (pair, result, open_price, current_price, current_time))
-            if result == "CLOSE":
-                LOGGER.debug("Items to close")
-                closes.append((pair, current_time, current_price, event, 0))
-                drawdowns[pair] = redis.get_drawdown(pair)['perc']
-                drawups[pair] = redis.get_drawup(pair)['perc']
-                if alert:
-                    payload = {"pair":pair, "strategy":"alert", "host": "alert",
-                               "text": "Closing API trade according to TP/SL rules",
-                               "action":"close"}
-                    url = "http://router:1080/{}".format(config.web.api_token)
-                    try:
-                        requests.post(url, json=payload, timeout=1)
-                    except Exception:
-                        pass
+                LOGGER.debug("%s int check result: %s Open:%s Current:%s Time:%s"
+                             % (pair, result, open_price, current_price, current_time))
+                if result == "CLOSE":
+                    LOGGER.debug("Items to close")
+                    closes.append((pair, current_time, current_price, event, 0))
+                    drawdowns[pair] = redis.get_drawdown(pair)['perc']
+                    drawups[pair] = redis.get_drawup(pair)['perc']
+                    if alert:
+                        payload = {"pair":pair, "strategy":"alert", "host": "alert",
+                                   "text": "Closing API trade according to TP/SL rules",
+                                   "action":"close"}
+                        url = "http://router:1080/{}".format(config.web.api_token)
+                        try:
+                            requests.post(url, json=payload, timeout=1)
+                        except Exception:
+                            pass
 
-                trade = Trade(interval=interval, test_trade=test, test_data=False, config=config)
-                trade.close_trade(closes, drawdowns=drawdowns, drawups=drawups)
+                    trade = Trade(interval=interval, test_trade=test,
+                                  test_data=False, config=config)
+                    trade.close_trade(closes, drawdowns=drawdowns, drawups=drawups)
 
         del redis
         del dbase
