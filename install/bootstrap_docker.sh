9272ff1e55084fd5912446ef23df5ee48e0c56e9
Amro Diab <adiab@hotmail.co.uk>
2023-07-07 10:38:42 +0100

    remove pip hackyness and install req'd ver in base image

--- install/bootstrap_docker.sh
+++ install/bootstrap_docker.sh
@@ -14,9 +14,4 @@ python ./setup.py install
 [[ ! -d /opt/output ]] && mkdir /opt/output
 apt-get purge -y gcc g++ g++-8 gcc-8 libx265-165 mercurial-common || true
 apt-get -y autoremove
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-pip install --upgrade setuptools==45.1.0
-pip install pip==9.0.1
-rm get-pip.py
 echo "Installation Complete"
