5d8889741edd45276f5ac042593516cf92ee6646
Amro Diab <adiab@hotmail.co.uk>
2023-07-24 09:49:00 +0100

    don't forward every trade

--- greencandle/bin/api_router.py
+++ greencandle/bin/api_router.py
@@ -107,8 +107,12 @@ def respond():
             new_env, new_strategy = container.split(':')
             payload['env'] = new_env
             payload['strategy'] = new_strategy
-        payload['host'] = '10.8.0.1'
-        forward(payload)
+            payload['host'] = '10.8.0.1'
+            forward(payload)
+        else:
+            payload['host'] = '10.8.0.1'
+            forward(payload)
+
     mysql = Mysql()
     try:
         mysql.insert_api_trade(**request.json)
