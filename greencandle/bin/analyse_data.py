be37014ed32a3919e1856127cf9187a24e5a6f50
Amro Diab <adiab@hotmail.co.uk>
2023-07-27 17:53:36 +0100

    remove EMA rules, leaving only supertrend for 2nd level data

--- greencandle/bin/analyse_data.py
+++ greencandle/bin/analyse_data.py
@@ -76,7 +76,7 @@ def get_match_name(matches):
     """
     match_names = []
     container_num = int(config.main.name[-1])
-    name_lookup = [['ema', 'stx'],['distance', 'bb']]
+    name_lookup = [['stx'],['distance', 'bb']]
     for match in matches:
         match_names.append(name_lookup[container_num-1][match-1])
     return ','.join(match_names)
