1226b11d1ee80d979b4720f4c8ee15fa8d9fe92b
Amro Diab <adiab@hotmail.co.uk>
2023-08-11 16:33:16 +0100

    don't ignore last item in agg

--- greencandle/lib/aggregate_data.py
+++ greencandle/lib/aggregate_data.py
@@ -270,7 +270,7 @@ def collect_agg_data(interval):
     for pair in pairs:
         try:
             items[interval][pair] = redis.get_items(pair=pair,
-                                                    interval=interval)[-int(samples):-1]
+                                                    interval=interval)[:int(samples)]
         except:
             continue
 
