f22c5966fc6ae25feda38e8dffc2e5e2535ccc6c
Amro Diab <adiab@hotmail.co.uk>
2023-08-13 15:59:33 +0100

    add HA match name to analyser list

--- greencandle/bin/analyse_data.py
+++ greencandle/bin/analyse_data.py
@@ -83,7 +83,8 @@ def get_match_name(matches):
 
     name_lookup = [['stx'],
                    ['distance', 'bb'],
-                   ['candle_low', 'candle_zero', 'candle_very_low']
+                   ['candle_low', 'candle_zero', 'candle_very_low'],
+                   ['HA']
                    ]
     for match in matches:
         match_names.append(name_lookup[container_num-1][match-1])
