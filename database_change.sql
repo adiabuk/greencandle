74dca3bc5bcbf31be65ce7ea4cb2718496b9356d
Amro Diab <adiab@hotmail.co.uk>
2023-08-30 09:18:27 +0100

    db table to hold commission payments

--- database_change.sql
+++ database_change.sql
@@ -1,1 +1,13 @@
 # 6.30
+
+drop table if exists commission_paid;
+CREATE TABLE `commission_paid` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT current_timestamp(),
+  `asset` varchar(30) DEFAULT NULL,
+  `usd_amt` decimal(15,2),
+  `gbp_amt` decimal(15,2),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39669 DEFAULT CHARSET=latin1;
+
+
