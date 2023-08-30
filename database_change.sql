bf689a31bdd96bf07f659e55dae2947689508695
Amro Diab <adiab@hotmail.co.uk>
2023-08-30 16:29:51 +0100

    cleanup profit monthly db view

--- database_change.sql
+++ database_change.sql
@@ -1,5 +1,6 @@
 # 6.30
 
+# add new commision table
 drop table if exists commission_paid;
 CREATE TABLE `commission_paid` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -11,3 +12,8 @@ CREATE TABLE `commission_paid` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=39670 DEFAULT CHARSET=latin1;
 
+
+# cleanup profit_monthly view
+drop view if exists profit_monthly;
+create view profit_monthly as
+select left(`profit`.`close_time`,7) AS `date`,sum(`profit`.`usd_profit`) AS `usd_profit`,sum(`profit`.`usd_net_profit`) AS `usd_net_profit`,sum(`profit`.`perc`) AS `perc`,sum(`profit`.`net_perc`) AS `net_perc`,count(0) AS `count` from `greencandle`.`profit` where `profit`.`perc` is not null and year(close_time) != '0000' group by left(`profit`.`close_time`,7) order by left(`profit`.`close_time`,7) desc,sum(`profit`.`quote_profit`);
