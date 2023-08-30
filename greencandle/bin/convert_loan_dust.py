3372a06d9a0a2edfbbcc537c504e17b7e42cec63
Amro Diab <adiab@hotmail.co.uk>
2023-08-30 07:30:11 +0100

    cleanup script and add comments

--- greencandle/bin/convert_loan_dust.py
+++ greencandle/bin/convert_loan_dust.py
@@ -18,22 +18,31 @@ def main():
     cross_details = client.get_cross_margin_details()
     borrowed_set = set()
 
+    # get unique set of assets which have a loan against tem
     for item in cross_details['userAssets']:
         if float(item['borrowed']) > 0:
             borrowed_set.add(item['asset'])
-            #borrowed_set.add((item['asset'], item['borrowed']))
 
     dbase = Mysql()
     open_trades = dbase.fetch_sql_data('select pair, direction from open_trades',
                                        header=False)
+    # get unique set of pairs with open trades
+    # get assets from pairs, base if short, quote if long
     open_set = set()
     for pair, direction in open_trades:
         if direction == 'short':
             open_set.add(get_base(pair))
         else:
             open_set.add(get_quote(pair))
+
+    # get assets with loan which are not in a trade
     main_set = set(borrowed_set - open_set)
+
+    # get set of assets which we can convert (smaller than 10USDT)
     redeemable = client.get_small_liability_set()
+
+    # get final list of assets which can be redeemed
+    # and are in our earlier set
     final = list(main_set.intersection(redeemable))
 
     li_of_lis = []
@@ -42,8 +51,12 @@ def main():
     step = 10
     for item in range(start, end, step):
         li_of_lis.append(final[item:item+step])
-
+    # get list of lists containing assets to convert
+    # maximum 10 assets per list
+    # maximum 3 lists
+    # The rest will be discarded, likely picked up on next run
     for current_li in li_of_lis[:3]:
         client.small_liability_exchange(current_li)
+
 if __name__ == '__main__':
     main()
