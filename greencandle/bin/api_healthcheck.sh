e40c7a561e3aa0415494b5b1764fc938a2673dc3
Amro Diab <adiab@hotmail.co.uk>
2023-08-31 14:29:32 +0100

    add curl to backend_api healthcheck

--- greencandle/bin/api_healthcheck.sh
+++ greencandle/bin/api_healthcheck.sh
@@ -4,8 +4,11 @@
 # Need 2 procs running: backend_api-api.* and rq*
 
 count=$(ps -eocommand|grep -E 'backend_api-api|^rq'|grep -v grep|wc -l)
-
-if [[ "$count" < 2 ]]; then
+api_health=$(curl -Isf 127.1:20000/healthcheck &>/dev/null; echo $?)
+if [[ "$api_health" != 0 ]]; then
+    echo "failing healthcheck curl"
+    exit 1
+elif [[ "$count" < 2 ]]; then
     echo "not enough procs: $count"
     exit 1
 elif [[ "$count" > 2 ]]; then
