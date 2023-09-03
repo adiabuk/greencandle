4401b1f96f15d03b88135ba4dc876ab83b85d819
Amro Diab <adiab@hotmail.co.uk>
2023-09-03 20:40:14 +0100

    allow redis cleanup script to take num arg

--- greencandle/bin/cleanup_redis.py
+++ greencandle/bin/cleanup_redis.py
@@ -4,7 +4,7 @@
 """
 Redis cleanup-script
 """
-
+import sys
 from greencandle.lib.redis_conn import Redis
 from greencandle.lib.logger import get_logger
 from greencandle.lib.common import arg_decorator
@@ -13,7 +13,7 @@ from greencandle.lib import config
 @arg_decorator
 def main():
     """
-    Cleanup old data from redis by all but the last 250 timeframes
+    Cleanup old data from redis by all but the last x timeframes
     for all intervals
     Run through cron, using pairs available in redis
     """
@@ -22,12 +22,13 @@ def main():
     pairs = config.main.pairs.split()
     config.create_config()
     count = 0
-    for interval in ['1m', '3m', '5m', '15m', '30m', '1h', '4h', '1d']:
+    num = sys.argv[1] if len(sys.argv) > 1 else 500
+    for interval in ['1m', '3m', '5m', '15m', '30m', '1h', '4h', '12h', '1d']:
         for pair in pairs:
             pair = pair.strip()
-            logger.info("Analysing pair %s:%s", pair, interval)
+            logger.debug("Analysing pair %s:%s", pair, interval)
             items = redis.get_items(pair, interval)
-            for item in items[:-500]:
+            for item in items[:-num]:
                 redis.conn.hdel(f'{pair}:{interval}', item)
                 count += 1
     logger.info("Deleted %s keys", count)
