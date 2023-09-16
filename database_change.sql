d0471dd9a5dfd9a6f3ae80015378326fa6256e8c
Amro Diab <adiab@hotmail.co.uk>
2023-09-16 18:22:53 +0100

    fix mysql char set

--- database_change.sql
+++ database_change.sql
@@ -1,1 +1,6 @@
 # 6.39
+
+# Fix character set
+SET collation_connection = 'utf8_general_ci';
+ALTER DATABASE greencandle CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE trades CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
