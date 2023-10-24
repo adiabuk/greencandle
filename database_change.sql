cdc98425793ea08d2bfe8ab5a8e55db45e9ad7c5
Amro Diab <adiab@hotmail.co.uk>
2023-10-24 12:05:32 +0100

    add day of week to profit_daily db view

--- database_change.sql
+++ database_change.sql
@@ -1,1 +1,4 @@
 # 6.40
+
+CREATE OR REPLACE VIEW profit_daily as
+select cast(`profit`.`close_time` as date) AS `date(close_time)`,dayname(`profit`.`close_time`) AS `dayname(close_time)`,sum(cast(`profit`.`net_perc` as decimal(12,2))) AS `sum_net_perc`,avg(cast(`profit`.`net_perc` as decimal(12,2))) AS `avg_net_perc`,max(cast(`profit`.`net_perc` as decimal(12,2))) AS `max_net_perc`,min(cast(`profit`.`net_perc` as decimal(12,2))) AS `min_net_perc`,count(0) AS `count`,sum(case when `profit`.`net_perc` < 0 then 1 else 0 end) AS `net_loss`,sum(case when `profit`.`net_perc` > 0 then 1 else 0 end) / count(0) * 100 AS `perc_profitable`,sum(case when `profit`.`net_perc` > 0 then 1 else 0 end) / count(0) * 100 AS `net_perc_profitable`,sum(cast(`profit`.`usd_net_profit` as decimal(12,2))) AS `usd_net_profit` from `greencandle`.`profit` group by cast(`profit`.`close_time` as date) order by cast(`profit`.`close_time` as date) desc;
