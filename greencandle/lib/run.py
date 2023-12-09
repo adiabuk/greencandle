703cfe38c04ed99e7eb43f9005dbdfc1cdd9acac
Amro Diab <adiab@hotmail.co.uk>
2023-12-09 16:40:47 +0000

    detect non-existent df, as well as empty

--- greencandle/lib/run.py
+++ greencandle/lib/run.py
@@ -346,7 +346,8 @@ class ProdRunner():
             # skip pair if empty dataframe (no new trades in kline)
             if len(new_dataframes[pair]) == 0:
                 continue
-            if len(self.dataframes[pair]) == 0:
+
+            if pair not in self.dataframes or len(self.dataframes[pair]) == 0:
 
                 df2 = self.dataframes[pair].append(pandas.Series(data['recent'][pair]),
                                                    ignore_index=True,
