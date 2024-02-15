8e1be45e97671ebd407348099840db14cddb969e
Amro Diab <adiab@hotmail.co.uk>
2024-02-15 17:31:03 +0000

    new loans db table, for pre-borrow

--- database_change.sql
+++ database_change.sql
@@ -1,1 +1,14 @@
-# 6.47
+# 6.59
+
+# table for loans taken before trade open
+
+drop table if exists extra_loans;
+CREATE TABLE IF NOT EXISTS extra_loans (
+    `id` int(	11)	NOT NULL AUTO_INCREMENT,
+	`symbol` varchar(6)	DEFAULT NULL,
+	`date_added` datetime DEFAULT current_timestamp(),
+	`date_removed` datetime DEFAULT NULL,
+	`borrowed` varchar(30) 	DEFAULT '0',
+	`borrowed_usd` varchar(	30)	DEFAULT NULL,
+	PRIMARY KEY (`id`)
+	);
