883200e2410f1e2a355819f544c737952c2d673d
Amro Diab <adiab@hotmail.co.uk>
2024-05-08 18:09:08 +0100

    new data rule for preempting trade

--- greencandle/bin/analyse_data.py
+++ greencandle/bin/analyse_data.py
@@ -122,7 +122,7 @@ def get_match_name(matches):
         container_num = 1
 
     name_lookup = [['trend_200', 'golden_cross', 'over_ma'],
-                   ['approach'],
+                   ['big_change'],
                    ['distance', 'bb', "bbperc_diff", "bbperc_extreme"],
                    ['bb_confirm'],
                    ['MACD_slowdown'],
