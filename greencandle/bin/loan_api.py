0eaddaaf77e6ddaafe2549c15c241132dc00684a
Amro Diab <adiab@hotmail.co.uk>
2024-05-19 21:23:32 +0100

    determine max loan amount for trade received

--- greencandle/bin/loan_api.py
+++ greencandle/bin/loan_api.py
@@ -4,15 +4,17 @@
 """
 API for tracking which pairs aren't tradable overtime
 """
-import sys
 from flask import Flask, request, Response
 from setproctitle import setproctitle
 from greencandle.lib import config
 from greencandle.lib.common import arg_decorator
 from greencandle.lib.logger import get_logger
+from greencandle.lib.balance_common import get_quote, get_base
+from greencandle.lib.binance_accounts import base2quote
+from greencandle.lib.auth import binance_auth
+from greencandle.lib.binance import BinanceException
 
 config.create_config()
-TEST = bool(len(sys.argv) > 1 and sys.argv[1] == '--test')
 APP = Flask(__name__)
 LOGGER = get_logger(__name__)
 
@@ -21,7 +23,23 @@ def respond():
     """
     Default route to trade
     """
-    print(request.json)
+    data = request.json
+    LOGGER.info("Request received: %s", str(data))
+    client = binance_auth()
+    if int(data['action']) == -1:
+        asset = get_base(data['pair'])
+        direction = "short"
+    else:
+        asset = get_quote(data['pair'])
+        direction = "long"
+    try:
+        max_borrow = client.get_max_borrow(asset=asset)
+        max_usd_borrow = max_borrow if 'USD' in asset else base2quote(max_borrow, asset+'USDT')
+    except BinanceException:
+        LOGGER.warning("Binance excption - no funds available")
+    LOGGER.info("Borrow amount for %s %s is %s %s (%s USD)", data['pair'], direction, max_borrow,
+                asset, max_usd_borrow)
+
     return Response(status=200)
 
 @APP.route('/healthcheck', methods=["GET"])
