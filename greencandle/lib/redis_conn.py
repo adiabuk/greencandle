d025b5231d30632f74fdaf61ee7d26fd1f0d2405
Amro Diab <adiab@hotmail.co.uk>
2024-07-09 11:53:04 +0100

    allow more data in results

--- greencandle/lib/redis_conn.py
+++ greencandle/lib/redis_conn.py
@@ -650,7 +650,7 @@ class Redis():
             ind = split[1]+'_' +split[2].split(',')[0]
             ind_list.append(ind)
         items = self.get_items(pair, interval)
-        for i in range(-1, -5, -1): # from, to, increment
+        for i in range(-1, -6, -1): # from, to, increment
             # look backwards through last 3 items of redis data
             datax = AttributeDict()
             for indicator in ind_list:
