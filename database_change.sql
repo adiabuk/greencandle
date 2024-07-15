c79b0b0959de5f7dbda0a1ecfb62a02ac1824b47
Amro Diab <adiab@hotmail.co.uk>
2024-07-15 15:38:14 +0100

    remove applied db changes

--- database_change.sql
+++ database_change.sql
@@ -1,6 +1,2 @@
 # 7.12
 
-# more digits for drawdown/drawup perc required for upper timeframes
-
-ALTER TABLE trades MODIFY drawup_perc varchar(6);
-ALTER TABLE trades MODIFY drawdown_perc varchar(6);
