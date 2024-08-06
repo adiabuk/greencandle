f26a6e0d02d5441b4b39f846e8fa6808a1cc6989
Amro Diab <adiab@hotmail.co.uk>
2024-08-06 13:19:41 +0100

    cleanup loan api,add client output and get req

--- greencandle/bin/loan_api.py
+++ greencandle/bin/loan_api.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 #pylint: disable=no-member,no-name-in-module,broad-except
 
 """
@@ -8,7 +9,7 @@ import logging
 from flask import Flask, request, Response
 from setproctitle import setproctitle
 from greencandle.lib import config
-from greencandle.lib.common import arg_decorator
+from greencandle.lib.common import arg_decorator, format_usd
 from greencandle.lib.logger import get_logger
 from greencandle.lib.balance_common import get_quote, get_base
 from greencandle.lib.binance_accounts import base2quote
@@ -16,44 +17,48 @@ from greencandle.lib.auth import binance_auth
 from greencandle.lib.binance import BinanceException
 
 config.create_config()
-APP = Flask(__name__)
-LOGGER = get_logger(__name__)
+APP = Flask('loan_api')
+LOGGER = get_logger('loan_api')
 
-@APP.route('/webhook', methods=['POST'])
+@APP.route('/webhook', methods=['POST', 'GET'])
 def respond():
     """
     Default route to trade
     """
+    #if request.method == 'GET':
+
+    data = request.json if request.method == 'POST' else request.args
+    keys = ['pair', 'action']
+    if not all(key in data.keys() for key in keys):
+        response = f'Missing keys, provided: {str(data.keys())}, required: {str(keys)}'
+        LOGGER.warning(response)
+        return Response(response=response + '\n', status=500)
+
+    client = binance_auth()
+    if int(data['action']) == -1:
+        asset = get_base(data['pair'].upper())
+        direction = "short"
+    elif int(data['action']) == 1:
+        asset = get_quote(data['pair'].upper())
+        direction = "long"
+
+    LOGGER.debug("Request received: %s", str(data))
+
     try:
-        data = request.json
-        client = binance_auth()
-        if int(data['action']) == -1:
-            asset = get_base(data['pair'].upper())
-            direction = "short"
-        elif int(data['action']) == 1:
-            asset = get_quote(data['pair'].upper())
-            direction = "long"
+        max_usd_only_borrow = client.get_max_borrow(asset='USDT')
+        max_borrow = client.get_max_borrow(asset=asset)
+        max_usd_borrow = max_borrow if 'USD' in asset else base2quote(max_borrow, asset+'USDT')
+    except BinanceException:
+        max_borrow = 0
+        max_usd_borrow = 0
+        LOGGER.warning("Binance excption - no funds available")
+    origin = data['text'].split()[-3] if "text" in data else "unknown"
 
-        LOGGER.debug("Request received: %s", str(data))
-        if not direction == 'short':
-            LOGGER.debug("Skipping non-short trade")
-            return Response(status=200)
-
-        try:
-            max_usd_only_borrow = client.get_max_borrow(asset='USDT')
-            max_borrow = client.get_max_borrow(asset=asset)
-            max_usd_borrow = max_borrow if 'USD' in asset else base2quote(max_borrow, asset+'USDT')
-        except BinanceException:
-            max_borrow = 0
-            max_usd_borrow = 0
-            LOGGER.warning("Binance excption - no funds available")
-        origin = data['text'].split()[-3]
-        LOGGER.info("Borrow amount for %s %s is %s %s (%s USD), usd_only:%s, %s", data['pair'],
-                    direction, max_borrow, asset, max_usd_borrow, max_usd_only_borrow, origin)
-    except Exception as err:
-        LOGGER.warning("Error: %s", str(err))
-
-    return Response(status=200)
+    ret_str = (f"Max borrow amount for {data['pair']} {direction} is "
+               f"{format_usd(max_usd_borrow)},usd borrow:{format_usd(max_usd_only_borrow)},"
+               f"{origin}")
+    LOGGER.info(ret_str)
+    return ret_str + '\n'
 
 @APP.route('/healthcheck', methods=["GET"])
 def healthcheck():
@@ -69,7 +74,7 @@ def main():
     Receives trade requests from web front-end/API/router and
     fetching maximum loanable amount
     """
-    setproctitle(f"{config.main.base_env}-amount_api")
+    setproctitle(f"{config.main.base_env}-loan_api")
     logging.basicConfig(level=logging.ERROR)
     if float(config.main.logging_level) > 10:
         log = logging.getLogger('werkzeug')
