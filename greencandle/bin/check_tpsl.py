a05e9b5db05f8ef731f5918228d7953f54bda763
Amro Diab <adiab@hotmail.co.uk>
2024-11-13 17:08:39 +0000

    cron nagios alert for missing redis tpsl

---
+++ greencandle/bin/check_tpsl.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#pylint: disable=no-member
+"""
+missing tpsl alert for nagios
+"""
+from send_nsca3 import send_nsca
+from greencandle.lib.redis_conn import Redis
+from greencandle.lib.mysql import Mysql
+from greencandle.lib import config
+from greencandle.lib.common import arg_decorator
+
+
+@arg_decorator
+def main():
+    """
+    Find open trades that don't have an associated tpsl in redis
+    and forward to nagios/nsca
+    """
+    config.create_config()
+    redis = Redis()
+    dbase = Mysql()
+
+    trades = dbase.get_open_trades()
+    no_tpsl = []
+
+    for item in trades:
+        _, interval, pair, name, _, direction, _ = item
+        current_take = redis.get_on_entry(pair, 'take_profit_perc', name=name,
+                                          interval=interval, direction=direction)
+        if current_take == 0:
+            no_tpsl.append(pair)
+
+    if len(no_tpsl) > 0:
+        msg = "Some pairs have no tpsl in redis: " + " ".join(no_tpsl)
+        status = 2
+    else:
+        msg = "all pairs have tpsl"
+        status = 0
+    env = config.main.base_env
+    send_nsca(status=status, host_name='jenkins', service_name=f'{env}_tpsl', text_output=msg,
+              remote_host='10.0.0.212')
+
+
+if __name__ == '__main__':
+    main()
