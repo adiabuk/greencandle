6f63835e2e8c15e1e345089ab4167b7f3f118703
Amro Diab <adiab@hotmail.co.uk>
2025-01-10 13:33:48 +0000

    compare strategy with/without STOCH reversal

--- greencandle/bin/analyse_data.py
+++ greencandle/bin/analyse_data.py
@@ -142,7 +142,7 @@ def get_match_name(matches):
                    ['multi_ind'],
                    ['distance', 'bb', "bbperc_diff", "bbperc_extreme"],
                    ['empty4'],
-                   ['STOCHRSI_flip'],
+                   ['STOCHRSI_flip', 'RSI_close-rule'],
                    ['rsi'],
                    ['empty7'],
                    ['empty8'],
