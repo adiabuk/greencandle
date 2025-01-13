8c7c1e5b882f3e8de79ffd42dadeb60c5ab5e354
Amro Diab <adiab@hotmail.co.uk>
2025-01-13 16:47:37 +0000

    exclude 1m from error log alerts

--- greencandle/bin/logwatch.py
+++ greencandle/bin/logwatch.py
@@ -46,9 +46,9 @@ def check_last_hour_err():
         last_hour_date_time = datetime.now() - timedelta(hours = 1)
 
         if current > last_hour_date_time:
-            if any(status in line for status in ['CRIT', 'ERR', 'Traceback']):
+            if any(status in line for status in ['CRIT', 'ERR', 'Traceback'] and '1m' not in line):
                 err_count += 1
-            if "WARN" in line:
+            if "WARN" in line and '1m' not in line:
                 warn_count += 1
         else:
             break
