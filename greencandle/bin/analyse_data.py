117be399bfed200c00a3a100996ade44af8568ef
Amro Diab <adiab@hotmail.co.uk>
2025-03-19 19:58:55 +0000

    turn dicts into attributedicts

--- greencandle/bin/analyse_data.py
+++ greencandle/bin/analyse_data.py
@@ -28,6 +28,7 @@ from greencandle.lib.common import get_tv_link, arg_decorator, convert_to_second
 from greencandle.lib.auth import binance_auth
 from greencandle.lib.order import Trade
 from greencandle.lib.web import decorator_timer
+from greencandle.lib.objects import AttributeDict
 
 config.create_config()
 INTERVAL = config.main.interval
@@ -185,17 +186,14 @@ def analyse_pair(pair, reversal, expire, redis):
         return
 
     LOGGER.debug("analysing pair: %s", pair)
-    res = DATA[pair]['res']
-    items = DATA[pair]['items']
-    agg = DATA[pair]['agg']
+    res = AttributeDict(DATA[pair]['res'])
+    items = AttributeDict(DATA[pair]['items'])
+    agg = AttributeDict(DATA[pair]['agg'])
     sent = DATA[pair]['sent']
     output = redis.get_rule_action(pair=pair, interval=INTERVAL, res=res, agg=agg, sent=sent,
                                    items=items )
-    result, _, current_time, current_price, match = output
+    result, _, current_time, current_price, _ = output
     event = reversal
-    res = match['res']
-    sent = match['sent']
-    agg = match['agg']
     LOGGER.debug("analysis result for %s is %s", pair, str(output))
 
     if result in ('OPEN', 'CLOSE'):
