215568a358a14ecebaf967e5918a50a37b646ce2
Amro Diab <adiab@hotmail.co.uk>
2025-05-06 10:05:55 +0100

    use sentiment to eval if we are against trend

--- greencandle/bin/check_against_trend.py
+++ greencandle/bin/check_against_trend.py
@@ -7,11 +7,11 @@ using drain for interval/env and send to nagios
 
 import sys
 from send_nsca3 import send_nsca
-from greencandle.lib.web import get_drain
 from greencandle.lib.mysql import Mysql
 from greencandle.lib import config
 from greencandle.lib.logger import get_logger
 from greencandle.lib.common import arg_decorator
+from greencandle.lib.sentiment import Sentiment
 
 @arg_decorator
 def main():
@@ -25,15 +25,15 @@ def main():
     long_count = 0
     short_count = 0
 
-    for interval in ('15m', '1h'):
-        short_drain = get_drain(env, interval, 'short')
-        long_drain = get_drain(env, interval, 'long')
+    sentiment = Sentiment()
+    result = int(sentiment.get_tv('4h'))
+    for interval in ('15m', '1h', '30m', '4h'):
 
 
-        if short_drain:
+        if result > 0:
             count = len(dbase.get_open_trades(name_filter=interval, direction_filter='short'))
             short_count += count
-        if long_drain:
+        if result < 0:
             count = len(dbase.get_open_trades(name_filter=interval, direction_filter='long'))
             long_count += count
 
@@ -54,8 +54,8 @@ def main():
                           f'{long_count}, short: {short_count};total={total};;;;',
               remote_host='nagios.amrox.loc')
 
-    logger.info('%s  trades exist against long drain', long_count)
-    logger.info('%s trades exist against short drain', short_count)
+    logger.info('%s  trades exist against short trend', long_count)
+    logger.info('%s trades exist against long trend', short_count)
     sys.exit(status)
 
 
