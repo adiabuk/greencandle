bc005c7d47ac03ad5e6673f8827a502f55286908
Amro Diab <adiab@hotmail.co.uk>
2025-07-14 15:30:26 +0000

    more quote subs

--- greencandle/lib/mysql.py
+++ greencandle/lib/mysql.py
@@ -217,8 +217,8 @@ class Mysql():
         """
         kwargs = AttributeDict(kwargs)
         command = f"""insert into api_requests (pair, text, action, price, strategy) VALUES
-                     ("{kwargs.pair}", "{kwargs.text}", "{kwargs.action}",
-                     "{kwargs.get('price', 'N/A')}", "{kwargs.strategy}")"""
+                     ('{kwargs.pair}', '{kwargs.text}', '{kwargs.action}',
+                     '{kwargs.get("price", "N/A")}', '{kwargs.strategy}')"""
         result = self.__run_sql_query(command)
         return result == 1
 
